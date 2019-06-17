e13cd77e2f0eed5c7e4f613d046e4233146bdc24
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:11:52 +0200

    Copyright notice and clean up.

--- src/test/java/io/vlingo/schemata/codegen/CodeGenTests.java
+++ src/test/java/io/vlingo/schemata/codegen/CodeGenTests.java
@@ -1,3 +1,10 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.codegen;
 
 import java.io.BufferedReader;
@@ -6,14 +13,12 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public abstract class CodeGenTests {
-    protected final String typeDefinition(final String name) {
-        try {
-            final InputStream resource = getClass().getResourceAsStream("/io/vlingo/schemata/codegen/vss/" + name + ".vss");
-            return new BufferedReader(new InputStreamReader(resource))
-                    .lines()
-                    .collect(Collectors.joining("\n"));
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
+  protected final String typeDefinition(final String name) {
+    try {
+      final InputStream resource = getClass().getResourceAsStream("/io/vlingo/schemata/codegen/vss/" + name + ".vss");
+      return new BufferedReader(new InputStreamReader(resource)).lines().collect(Collectors.joining("\n"));
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
     }
+  }
 }
