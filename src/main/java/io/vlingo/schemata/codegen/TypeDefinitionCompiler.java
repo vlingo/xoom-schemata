72640c31864a5aab8bf2746d27e925a08cf2530d
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:06:52 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/codegen/TypeDefinitionCompiler.java
+++ src/main/java/io/vlingo/schemata/codegen/TypeDefinitionCompiler.java
@@ -1,31 +1,39 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.codegen;
 
-import io.vlingo.schemata.codegen.antlr.SchemaVersionDefinitionLexer;
-import io.vlingo.schemata.codegen.antlr.SchemaVersionDefinitionParser;
+import java.nio.ByteBuffer;
+
 import org.antlr.v4.runtime.CodePointBuffer;
 import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import java.nio.ByteBuffer;
+import io.vlingo.schemata.codegen.antlr.SchemaVersionDefinitionLexer;
+import io.vlingo.schemata.codegen.antlr.SchemaVersionDefinitionParser;
 
 public class TypeDefinitionCompiler {
-    private final Backend backend;
+  private final Backend backend;
 
-    private TypeDefinitionCompiler(Backend backend) {
-        this.backend = backend;
-    }
+  private TypeDefinitionCompiler(final Backend backend) {
+    this.backend = backend;
+  }
 
-    public static TypeDefinitionCompiler backedBy(final Backend backend) {
-        return new TypeDefinitionCompiler(backend);
-    }
+  public static TypeDefinitionCompiler backedBy(final Backend backend) {
+    return new TypeDefinitionCompiler(backend);
+  }
 
-    public String compile(final String typeDefinition) {
-        CodePointBuffer buffer = CodePointBuffer.withBytes(ByteBuffer.wrap(typeDefinition.getBytes()));
-        CodePointCharStream in = CodePointCharStream.fromBuffer(buffer);
-        SchemaVersionDefinitionLexer lexer = new SchemaVersionDefinitionLexer(in);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SchemaVersionDefinitionParser parser = new SchemaVersionDefinitionParser(tokens);
+  public String compile(final String typeDefinition) {
+    CodePointBuffer buffer = CodePointBuffer.withBytes(ByteBuffer.wrap(typeDefinition.getBytes()));
+    CodePointCharStream in = CodePointCharStream.fromBuffer(buffer);
+    SchemaVersionDefinitionLexer lexer = new SchemaVersionDefinitionLexer(in);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    SchemaVersionDefinitionParser parser = new SchemaVersionDefinitionParser(tokens);
 
-        return backend.generateFrom(parser.typeDeclaration());
-    }
+    return backend.generateFrom(parser.typeDeclaration());
+  }
 }
