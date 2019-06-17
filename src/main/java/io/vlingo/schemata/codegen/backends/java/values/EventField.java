e867285c08d374b911acbea209d0928b54c77a67
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:07:25 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/codegen/backends/java/values/EventField.java
+++ src/main/java/io/vlingo/schemata/codegen/backends/java/values/EventField.java
@@ -1,21 +1,30 @@
-package io.vlingo.schemata.codegen.backends.java.values;
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
 
-import io.vlingo.common.Tuple2;
+package io.vlingo.schemata.codegen.backends.java.values;
 
 import java.util.Optional;
 
+import io.vlingo.common.Tuple2;
+
 public abstract class EventField {
-    protected final String ownerClass;
-    protected final String typeName;
-    protected final String fieldName;
+  protected final String ownerClass;
+  protected final String typeName;
+  protected final String fieldName;
 
-    protected EventField(String ownerClass, String typeName, String fieldName) {
-        this.ownerClass = ownerClass;
-        this.typeName = typeName;
-        this.fieldName = fieldName;
-    }
+  protected EventField(final String ownerClass, final String typeName, final String fieldName) {
+    this.ownerClass = ownerClass;
+    this.typeName = typeName;
+    this.fieldName = fieldName;
+  }
 
-    public abstract Tuple2<Class<?>, String> fieldDefinition();
-    public abstract Optional<Tuple2<Class<?>, String>> parameterDefinition();
-    public abstract String initialization();
+  public abstract Tuple2<Class<?>, String> fieldDefinition();
+
+  public abstract Optional<Tuple2<Class<?>, String>> parameterDefinition();
+
+  public abstract String initialization();
 }
