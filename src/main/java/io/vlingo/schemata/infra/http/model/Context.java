2cb1e65e1ff9781da265fef0a6adb3cc5462a864
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:08:56 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/infra/http/model/Context.java
+++ src/main/java/io/vlingo/schemata/infra/http/model/Context.java
@@ -1,30 +1,35 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.infra.http.model;
 
 import io.vlingo.schemata.model.ContextEntity;
-import io.vlingo.schemata.model.UnitEntity;
 
 public class Context {
   public final String id;
   public final String name;
   public final String description;
 
-
-  private Context(String id, String name, String description) {
+  private Context(final String id, String name, final String description) {
     this.id = id;
     this.name = name;
     this.description = description;
   }
 
-  public static Context from(ContextEntity ce) {
+  public static Context from(final ContextEntity ce) {
     // TODO: Adapt from domain model to API model
     throw new UnsupportedOperationException();
   }
 
-  public static Context from(String id, String name) {
+  public static Context from(final String id, final String name) {
     return from(id, name, "");
   }
 
-  public static Context from(String id, String name, String description) {
+  public static Context from(final String id, final String name, final String description) {
     return new Context(id, name, description);
   }
 }
