c2a9f1f452f5654a50c462ad6620efe075860e3e
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:11:04 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/model/Unit.java
+++ src/main/java/io/vlingo/schemata/model/Unit.java
@@ -12,21 +12,21 @@ import io.vlingo.schemata.model.Id.OrganizationId;
 import io.vlingo.schemata.model.Id.UnitId;
 
 public interface Unit {
-    static UnitId uniqueId(final OrganizationId organizationId) {
-        return UnitId.uniqueFor(organizationId);
-    }
+  static UnitId uniqueId(final OrganizationId organizationId) {
+    return UnitId.uniqueFor(organizationId);
+  }
 
-    static Unit with(final Stage stage, final OrganizationId organizationId, final String name, final String description) {
-        return with(stage, uniqueId(organizationId), name, description);
-    }
+  static Unit with(final Stage stage, final OrganizationId organizationId, final String name, final String description) {
+    return with(stage, uniqueId(organizationId), name, description);
+  }
 
-    static Unit with(final Stage stage, final UnitId unitId, final String name, final String description) {
-        return stage.actorFor(Unit.class, UnitEntity.class, unitId, name, description);
-    }
+  static Unit with(final Stage stage, final UnitId unitId, final String name, final String description) {
+    return stage.actorFor(Unit.class, UnitEntity.class, unitId, name, description);
+  }
 
-    void defineWith(final String name, final String description);
+  void defineWith(final String name, final String description);
 
-    void describeAs(final String description);
+  void describeAs(final String description);
 
-    void renameTo(final String name);
+  void renameTo(final String name);
 }
