f3f201134eec4d91377d017d0e7ee0cdd7e9ada9
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:11:22 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/resource/SchemaResource.java
+++ src/main/java/io/vlingo/schemata/resource/SchemaResource.java
@@ -1,3 +1,10 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.resource;
 
 import static io.vlingo.http.resource.ResourceBuilder.get;
@@ -13,54 +20,32 @@ import io.vlingo.schemata.codegen.TypeDefinitionCompiler;
 import io.vlingo.schemata.codegen.backends.java.JavaCodeGenerator;
 
 public class SchemaResource {
-    @SuppressWarnings("rawtypes")
-    public static Resource asResource() {
-        SchemaResource impl = new SchemaResource();
+  @SuppressWarnings("rawtypes")
+  public static Resource asResource() {
+    SchemaResource impl = new SchemaResource();
 
-        return resource("schema", 10,
-                put("/{organization}/{context}/{unit}")
-                        .param(String.class)
-                        .param(String.class)
-                        .param(String.class)
-                        .body(String.class)
-                        .handle(impl::createSchema),
-                get("/{organization}/{context}/{unit}/{qualifiedName}")
-                        .param(String.class)
-                        .param(String.class)
-                        .param(String.class)
-                        .param(String.class)
-                        .header("Accept")
-                        .handle(impl::getSchema));
+    return resource("schema", 10,
+            put("/{organization}/{context}/{unit}").param(String.class).param(String.class).param(String.class)
+                    .body(String.class).handle(impl::createSchema),
+            get("/{organization}/{context}/{unit}/{qualifiedName}").param(String.class).param(String.class)
+                    .param(String.class).param(String.class).header("Accept").handle(impl::getSchema));
+  }
+
+  private String schemaDef;
+
+  private Completes<Response> createSchema(final String organization, final String context, final String unit, final String schemaDefinition) {
+    this.schemaDef = new String(schemaDefinition);
+
+    return Completes.withSuccess(Response.of(Response.Status.Created, Header.Headers.of(ResponseHeader.of("Location",
+            String.format("/%s/%s/%s/%s:%s", organization, context, unit, "name", "0.0.0")))));
+  }
+
+  private Completes<Response> getSchema(final String organization, final String context, final String unit, final String qualifiedName, final Header language) {
+    if (!language.value.equals("x-vlingo-schema/java")) {
+      return Completes.withSuccess(Response.of(Response.Status.BadRequest));
     }
 
-    private String schemaDef;
-
-    private Completes<Response> createSchema(
-            String organization,
-            String context,
-            String unit,
-            String schemaDefinition
-    ) {
-        this.schemaDef = new String(schemaDefinition);
-
-        return Completes.withSuccess(Response.of(Response.Status.Created,
-                Header.Headers.of(
-                        ResponseHeader.of("Location",
-                                String.format("/%s/%s/%s/%s:%s", organization, context, unit, "name", "0.0.0")))));
-    }
-
-    private Completes<Response> getSchema(
-            String organization,
-            String context,
-            String unit,
-            String qualifiedName,
-            Header language
-    ) {
-        if (!language.value.equals("x-vlingo-schema/java")) {
-            return Completes.withSuccess(Response.of(Response.Status.BadRequest));
-        }
-
-        String code = TypeDefinitionCompiler.backedBy(new JavaCodeGenerator()).compile(schemaDef);
-        return Completes.withSuccess(Response.of(Response.Status.Ok, code));
-    }
+    String code = TypeDefinitionCompiler.backedBy(new JavaCodeGenerator()).compile(schemaDef);
+    return Completes.withSuccess(Response.of(Response.Status.Ok, code));
+  }
 }
