f767f629f427343c96d3046c01e1f6b7a97c5539
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:09:49 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/infra/http/model/SchemaMetaData.java
+++ src/main/java/io/vlingo/schemata/infra/http/model/SchemaMetaData.java
@@ -1,27 +1,33 @@
-package io.vlingo.schemata.infra.http.model;
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
 
-import io.vlingo.schemata.model.SchemaEntity;
+package io.vlingo.schemata.infra.http.model;
 
 import java.util.List;
 
-public class SchemaMetaData {
+import io.vlingo.schemata.model.SchemaEntity;
+
+public class SchemaMetadata {
   public final String id;
   public final String name;
   public final List<Schema> versions;
 
-
-  private SchemaMetaData(String id, String name, List<Schema> versions) {
+  private SchemaMetadata(final String id, final String name, final List<Schema> versions) {
     this.id = id;
     this.name = name;
     this.versions = versions;
   }
 
-  public static SchemaMetaData from(SchemaEntity ve) {
+  public static SchemaMetadata from(final SchemaEntity ve) {
     // TODO: Adapt from domain model to API model
     throw new UnsupportedOperationException();
   }
 
-  public static SchemaMetaData from(String id, String name, List<Schema> versions) {
-    return new SchemaMetaData(id, name, versions);
+  public static SchemaMetadata from(final String id, final String name, final List<Schema> versions) {
+    return new SchemaMetadata(id, name, versions);
   }
 }
