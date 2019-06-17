82d98f01cb3f0f94c9f867c2df4d8e7c7e8a3a59
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:10:34 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/infra/http/model/Unit.java
+++ src/main/java/io/vlingo/schemata/infra/http/model/Unit.java
@@ -1,8 +1,15 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.infra.http.model;
 
-import io.vlingo.schemata.model.UnitEntity;
-
 import java.util.List;
+
+import io.vlingo.schemata.model.UnitEntity;
 
 public class Unit {
   public final String id;
@@ -10,24 +17,23 @@ public class Unit {
   public final String description;
   public final List<Context> contexts;
 
-
-  private Unit(String id, String name, String description, List<Context> contexts) {
+  private Unit(final String id, final String name, final String description, final List<Context> contexts) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.contexts = contexts;
   }
 
-  public static Unit from(UnitEntity ue) {
+  public static Unit from(final UnitEntity ue) {
     // TODO: Adapt from domain model to API model
     throw new UnsupportedOperationException();
   }
 
-  public static Unit from(String id, String name, List<Context> contexts) {
+  public static Unit from(final String id, final String name, final List<Context> contexts) {
     return from(id, name, "", contexts);
   }
 
-  public static Unit from(String id, String name, String description, List<Context> contexts) {
+  public static Unit from(final String id, final String name, final String description, final List<Context> contexts) {
     return new Unit(id, name, description, contexts);
   }
 }
