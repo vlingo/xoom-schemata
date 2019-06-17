56ff858afe19578e57dbc0c4c1c3974de66829b7
VaughnVernon <vaughn@forcomprehension.com>
2019-06-18 00:06:35 +0200

    Copyright notice and clean up.

--- src/main/java/io/vlingo/schemata/codegen/Backend.java
+++ src/main/java/io/vlingo/schemata/codegen/Backend.java
@@ -1,7 +1,14 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.codegen;
 
 import io.vlingo.schemata.codegen.antlr.SchemaVersionDefinitionParser;
 
 public interface Backend {
-    String generateFrom(SchemaVersionDefinitionParser.TypeDeclarationContext typeDeclaration);
+  String generateFrom(final SchemaVersionDefinitionParser.TypeDeclarationContext typeDeclaration);
 }
