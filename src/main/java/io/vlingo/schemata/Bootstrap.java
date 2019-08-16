43e34cb39c982fe892f0895c484e1dae7e3d95ad
VaughnVernon <vaughn@forcomprehension.com>
2019-08-16 01:55:20 -0700

    Moved some resources packages.

--- src/main/java/io/vlingo/schemata/Bootstrap.java
+++ src/main/java/io/vlingo/schemata/Bootstrap.java
@@ -12,10 +12,10 @@ import io.vlingo.http.resource.Configuration;
 import io.vlingo.http.resource.Resources;
 import io.vlingo.http.resource.Server;
 import io.vlingo.lattice.model.sourcing.SourcedTypeRegistry;
-import io.vlingo.schemata.infra.http.MockApiResource;
 import io.vlingo.schemata.infra.persistence.EntryAdapters;
-import io.vlingo.schemata.infra.ui.UiResource;
+import io.vlingo.schemata.resource.MockApiResource;
 import io.vlingo.schemata.resource.SchemaResource;
+import io.vlingo.schemata.resource.UiResource;
 import io.vlingo.symbio.store.journal.Journal;
 import io.vlingo.symbio.store.journal.inmemory.InMemoryJournalActor;
 
