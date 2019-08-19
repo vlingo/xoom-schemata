40dc8e735c7c8797df1d0cb36e5263b4486a72cc
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-08-19 23:15:26 +0200

    Add query check UT for organization

--- src/test/java/io/vlingo/schemata/infra/persistence/SchemataObjectStoreTest.java
+++ src/test/java/io/vlingo/schemata/infra/persistence/SchemataObjectStoreTest.java
@@ -9,25 +9,36 @@ package io.vlingo.schemata.infra.persistence;
 
 import io.vlingo.actors.World;
 import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.actors.testkit.TestUntil;
 import io.vlingo.common.Outcome;
 import io.vlingo.lattice.model.object.ObjectTypeRegistry;
 import io.vlingo.schemata.NoopDispatcher;
 import io.vlingo.schemata.model.Id.OrganizationId;
 import io.vlingo.schemata.model.OrganizationState;
+import io.vlingo.symbio.BaseEntry;
+import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.DataFormat;
 import io.vlingo.symbio.store.Result;
 import io.vlingo.symbio.store.StorageException;
 import io.vlingo.symbio.store.common.jdbc.Configuration;
 import io.vlingo.symbio.store.common.jdbc.hsqldb.HSQLDBConfigurationProvider;
+import io.vlingo.symbio.store.dispatch.Dispatchable;
+import io.vlingo.symbio.store.object.ListQueryExpression;
+import io.vlingo.symbio.store.object.MapQueryExpression;
 import io.vlingo.symbio.store.object.ObjectStore;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SchemataObjectStoreTest {
+
+    private NoopDispatcher dispatcher;
     private ObjectStore objectStore;
     private World world;
 
@@ -41,18 +52,45 @@ public class SchemataObjectStoreTest {
         final TestPersistResultInterest persistInterest = new TestPersistResultInterest();
         final AccessSafely access = persistInterest.afterCompleting(1);
         final OrganizationState organizationState = OrganizationState.from(
-                    OrganizationId.existing("A343"))
-                .define("Vlingo", "Organization Vlingo");
+                1L,
+                OrganizationId.existing("A343"),
+                "Vlingo", "Organization Vlingo");
         objectStore.persist(organizationState, persistInterest);
         final Outcome<StorageException, Result> outcome = access.readFrom("outcome");
         assertEquals(Result.Success, outcome.andThen(success -> success).get());
+
+        final TestQueryResultInterest queryInterest = new TestQueryResultInterest();
+
+        // Map
+        queryInterest.until = TestUntil.happenings(1);
+        objectStore.queryObject(
+                MapQueryExpression.using(
+                        OrganizationState.class,
+                        "SELECT * FROM ORGANIZATION WHERE id = :id",
+                        MapQueryExpression.map("id", 1L)),
+                queryInterest);
+        queryInterest.until.completes();
+        assertNotNull(queryInterest.singleResult.get());
+        assertEquals(organizationState, queryInterest.singleResult.get().stateObject);
+
+        // List
+        queryInterest.until = TestUntil.happenings(1);
+        objectStore.queryObject(
+                ListQueryExpression.using(
+                        OrganizationState.class,
+                        "SELECT * FROM ORGANIZATION WHERE id = <listArgValues>",
+                        Arrays.asList(1L)),
+                queryInterest);
+        queryInterest.until.completes();
+        assertNotNull(queryInterest.singleResult.get());
+        assertEquals(organizationState, queryInterest.singleResult.get().stateObject);
     }
 
     @Before
     public void setUp() throws Exception {
         world = World.startWithDefaults("test-store");
 
-        final NoopDispatcher dispatcher = new NoopDispatcher();
+        dispatcher = new NoopDispatcher();
 
         final Configuration configuration = HSQLDBConfigurationProvider.testConfiguration(DataFormat.Native);
         final SchemataObjectStore schemataObjectStore = new SchemataObjectStore(configuration);
