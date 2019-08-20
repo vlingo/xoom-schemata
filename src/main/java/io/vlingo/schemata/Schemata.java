169520cb18ad1f8689473e3141d870c7d766a258
VaughnVernon <vaughn@forcomprehension.com>
2019-08-20 16:18:11 -0700

    Initial revision of Schemata constants.

---
+++ src/main/java/io/vlingo/schemata/Schemata.java
@@ -0,0 +1,30 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.schemata;
+
+public class Schemata {
+  //==========================
+  // IDs
+  //==========================
+
+  public static final String NoId = "(id?)";
+
+  //==========================
+  // Paths
+  //==========================
+
+  public static final String OrganizationsPath = "/organizations/{0}";
+
+  public static final String UnitsPath = "/organizations/{0}/units/{1}";
+
+  //==========================
+  // Names
+  //==========================
+
+  public static final String StageName = "vlingo-schemata-grid";
+}
