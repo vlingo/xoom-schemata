d61edd6998748f4e564a48c96535a5233bbb15bf
VaughnVernon <vaughn@forcomprehension.com>
2019-08-21 00:51:34 -0700

    Improved identifications.

--- src/main/java/io/vlingo/schemata/resource/UnitCommands.java
+++ src/main/java/io/vlingo/schemata/resource/UnitCommands.java
@@ -14,6 +14,7 @@ import io.vlingo.lattice.router.CommandDispatcher;
 import io.vlingo.lattice.router.CommandRouter;
 import io.vlingo.lattice.router.CommandRouter.Type;
 import io.vlingo.lattice.router.RoutableCommand;
+import io.vlingo.schemata.model.Id.UnitId;
 import io.vlingo.schemata.model.Unit;
 import io.vlingo.schemata.model.UnitEntity;
 import io.vlingo.schemata.model.UnitState;
@@ -28,7 +29,7 @@ class UnitCommands {
   }
 
   RoutableCommand<Unit,Command,UnitState> describeAs(
-          final String address,
+          final UnitId unitId,
           final String description) {
 
     final DescribeAs describeAs = new DescribeAs(description);
@@ -37,7 +38,8 @@ class UnitCommands {
             RoutableCommand
               .speaks(Unit.class)
               .to(UnitEntity.class)
-              .at(address)
+              .at(unitId.value)
+              .named(Unit.nameFrom(unitId))
               .delivers(describeAs)
               .answers(Completes.using(stage.scheduler()))
               .handledBy(describeAs);
@@ -47,8 +49,30 @@ class UnitCommands {
     return command;
   }
 
+  RoutableCommand<Unit,Command,UnitState> defineWith(
+          final UnitId unitId,
+          final String name,
+          final String description) {
+
+    final DefineWith defineWith = new DefineWith(name, description);
+
+    RoutableCommand<Unit,Command,UnitState> command =
+            RoutableCommand
+              .speaks(Unit.class)
+              .to(UnitEntity.class)
+              .at(unitId.value)
+              .named(Unit.nameFrom(unitId))
+              .delivers(defineWith)
+              .answers(Completes.using(stage.scheduler()))
+              .handledBy(defineWith);
+
+    router.route(command);
+
+    return command;
+  }
+
   RoutableCommand<Unit,Command,UnitState> renameTo(
-          final String address,
+          final UnitId unitId,
           final String name) {
 
     final RenameTo renameTo = new RenameTo(name);
@@ -57,7 +81,8 @@ class UnitCommands {
             RoutableCommand
               .speaks(Unit.class)
               .to(UnitEntity.class)
-              .at(address)
+              .at(unitId.value)
+              .named(Unit.nameFrom(unitId))
               .delivers(renameTo)
               .answers(Completes.using(stage.scheduler()))
               .handledBy(renameTo);
@@ -65,6 +90,24 @@ class UnitCommands {
     router.route(command);
 
     return command;
+  }
+
+  private static class DefineWith extends Command implements CommandDispatcher<Unit,DefineWith,Completes<UnitState>> {
+    private final String description;
+    private final String name;
+
+    DefineWith(final String name, final String description) {
+      this.name = name;
+      this.description = description;
+    }
+
+    @Override
+    public void accept(final Unit protocol, final DefineWith command, final Completes<UnitState> answer) {
+      protocol
+        .defineWith(command.name, command.description)
+        .andThen(state -> answer.with(state))
+        .otherwise(state -> answer.otherwise(none -> none));
+    }
   }
 
   private static class DescribeAs extends Command implements CommandDispatcher<Unit,DescribeAs,Completes<UnitState>> {
