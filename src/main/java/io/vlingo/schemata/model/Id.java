13834ef6e98325a3e1d7f6630e3f0520766dac2c
VaughnVernon <vaughn@forcomprehension.com>
2019-08-21 00:47:47 -0700

    Refactorings and refinements.

--- src/main/java/io/vlingo/schemata/model/Id.java
+++ src/main/java/io/vlingo/schemata/model/Id.java
@@ -56,6 +56,10 @@ public abstract class Id {
       return new UnitId(new OrganizationId(parts[0]), parts[1]);
     }
 
+    public static UnitId existing(final String organizationId, final String unitId) {
+      return new UnitId(OrganizationId.existing(organizationId), unitId);
+    }
+
     public static UnitId undefined() {
       return new UnitId(OrganizationId.undefined());
     }
@@ -77,9 +81,13 @@ public abstract class Id {
   public static class ContextId extends Id {
     public final UnitId unitId;
 
-    public static ContextId existing(final String id) {
-      final String[] parts = id.split(Separator);
+    public static ContextId existing(final String compositeId) {
+      final String[] parts = compositeId.split(Separator);
       return new ContextId(new UnitId(OrganizationId.existing(parts[0]), parts[1]), parts[2]);
+    }
+
+    public static ContextId existing(final String organizationId, final String unitId, final String contextId) {
+      return new ContextId(organizationId, unitId, contextId);
     }
 
     public static ContextId undefined() {
@@ -90,22 +98,35 @@ public abstract class Id {
       return new ContextId(unitId);
     }
 
-    private ContextId(final UnitId unitId) {
+    public OrganizationId organizationId() {
+      return this.unitId.organizationId;
+    }
+
+    ContextId(final UnitId unitId) {
       this(unitId, IdentityGeneratorType.Random.generate().toString());
     }
 
-    private ContextId(final UnitId unitId, final String contextIdValue) {
+    ContextId(final UnitId unitId, final String contextIdValue) {
       super(contextIdValue);
       this.unitId = unitId;
+    }
+
+    ContextId(final String organizationId, final String unitId, final String contextId) {
+      super(contextId);
+      this.unitId = UnitId.existing(organizationId, unitId);
     }
   }
 
   public static class SchemaId extends Id {
     public final ContextId contextId;
 
-    public static SchemaId existing(final String id) {
-      final String[] parts = id.split(Separator);
+    public static SchemaId existing(final String compositeId) {
+      final String[] parts = compositeId.split(Separator);
       return new SchemaId(new ContextId(new UnitId(OrganizationId.existing(parts[0]), parts[1]), parts[2]), parts[3]);
+    }
+
+    public static SchemaId existing(final String organizationId, final String unitId, final String contextId, final String schemaId) {
+      return new SchemaId(organizationId, unitId, contextId, schemaId);
     }
 
     public static SchemaId undefined() {
@@ -116,6 +137,14 @@ public abstract class Id {
       return new SchemaId(contextId);
     }
 
+    public OrganizationId organizationId() {
+      return this.contextId.organizationId();
+    }
+
+    public UnitId unitId() {
+      return this.contextId.unitId;
+    }
+
     SchemaId(final ContextId contextId) {
       this(contextId, IdentityGeneratorType.Random.generate().toString());
     }
@@ -123,6 +152,11 @@ public abstract class Id {
     SchemaId(final ContextId contextId, final String schemaIdValue) {
       super(schemaIdValue);
       this.contextId = contextId;
+    }
+
+    SchemaId(final String organizationId, final String unitId, final String contextId, final String schemaId) {
+      super(schemaId);
+      this.contextId = ContextId.existing(organizationId, unitId, contextId);
     }
   }
 
@@ -151,13 +185,30 @@ public abstract class Id {
       return new SchemaVersionId(schemaId, String.valueOf(previousSequence + 1));
     }
 
-    public SchemaVersionId(final SchemaId schemaId) {
+    public ContextId contextId() {
+      return this.schemaId.contextId;
+    }
+
+    public OrganizationId organizationId() {
+      return this.schemaId.organizationId();
+    }
+
+    public UnitId unitId() {
+      return this.schemaId.unitId();
+    }
+
+    SchemaVersionId(final SchemaId schemaId) {
       this(schemaId, IdentityGeneratorType.Random.generate().toString());
     }
 
-    public SchemaVersionId(final SchemaId schemaId, final String value) {
+    SchemaVersionId(final SchemaId schemaId, final String value) {
       super(value);
       this.schemaId = schemaId;
+    }
+
+    SchemaVersionId(final String organizationId, final String unitId, final String contextId, final String schemaId, final String version) {
+      super(version);
+      this.schemaId = SchemaId.existing(organizationId, unitId, contextId, schemaId);
     }
   }
 }
