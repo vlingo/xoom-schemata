88f298742fd1ff46ed11d93a3ea379d68921e224
VaughnVernon <vaughn@forcomprehension.com>
2019-08-22 20:15:42 -0700

    Routes and example queries.

--- src/main/java/io/vlingo/schemata/resource/ContextResource.java
+++ src/main/java/io/vlingo/schemata/resource/ContextResource.java
@@ -14,9 +14,15 @@ import static io.vlingo.http.Response.Status.Ok;
 import static io.vlingo.http.ResponseHeader.Location;
 import static io.vlingo.http.ResponseHeader.headers;
 import static io.vlingo.http.ResponseHeader.of;
+import static io.vlingo.http.resource.ResourceBuilder.get;
+import static io.vlingo.http.resource.ResourceBuilder.patch;
+import static io.vlingo.http.resource.ResourceBuilder.post;
+import static io.vlingo.http.resource.ResourceBuilder.resource;
 import static io.vlingo.schemata.Schemata.ContextsPath;
 import static io.vlingo.schemata.Schemata.NoId;
 import static io.vlingo.schemata.Schemata.StageName;
+
+import java.util.Arrays;
 
 import io.vlingo.actors.Stage;
 import io.vlingo.actors.World;
@@ -24,6 +30,7 @@ import io.vlingo.common.Completes;
 import io.vlingo.http.Header.Headers;
 import io.vlingo.http.Response;
 import io.vlingo.http.ResponseHeader;
+import io.vlingo.http.resource.Resource;
 import io.vlingo.http.resource.ResourceHandler;
 import io.vlingo.schemata.model.Context;
 import io.vlingo.schemata.model.Id.ContextId;
@@ -61,6 +68,47 @@ public class ContextResource extends ResourceHandler {
     return commands
             .moveToNamespace(ContextId.existing(organizationId, unitId, contextId), namespace).answer()
             .andThenTo(state -> Completes.withSuccess(Response.of(Ok, serialized(ContextData.from(state)))));
+  }
+
+  public Completes<Response> queryContexts(final String organizationId, final String unitId) {
+    System.out.println("***** QUERY ORG: " + organizationId + " UNIT: " + unitId + " CONTEXTS");
+    return Completes.withSuccess(Response.of(Ok, serialized(Arrays.asList(ContextData.from("O1", "U1", "C1", "Context1", "My context 1.")))));
+  }
+
+  public Completes<Response> queryContext(final String organizationId, final String unitId, final String contextId) {
+    System.out.println("***** QUERY ORG: " + organizationId + " UNIT: " + unitId + " CONTEXT: " + contextId);
+    return Completes.withSuccess(Response.of(Ok, serialized(ContextData.from("O1", "U1", "C1", "Context1", "My context 1."))));
+  }
+
+  @Override
+  public Resource<?> routes() {
+    return resource("Context Resource",
+      post("/organizations/{organizationId}/units/{unitId}/contexts")
+        .param(String.class)
+        .param(String.class)
+        .body(ContextData.class)
+        .handle(this::defineWith),
+      patch("/organizations/{organizationId}/units/{unitId}/contexts/{contextId}/description")
+        .param(String.class)
+        .param(String.class)
+        .param(String.class)
+        .body(String.class)
+        .handle(this::describeAs),
+      patch("/organizations/{organizationId}/units/{unitId}/contexts/{contextId}/namespace")
+        .param(String.class)
+        .param(String.class)
+        .param(String.class)
+        .body(String.class)
+        .handle(this::moveToNamespace),
+      get("/organizations/{organizationId}/units/{unitId}/contexts")
+        .param(String.class)
+        .param(String.class)
+        .handle(this::queryContexts),
+      get("/organizations/{organizationId}/units/{unitId}/contexts/{contextId}")
+        .param(String.class)
+        .param(String.class)
+        .param(String.class)
+        .handle(this::queryContext));
   }
 
   private String contextLocation(final ContextId contextId) {
