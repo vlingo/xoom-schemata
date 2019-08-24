7100982cfca0d15f52259a2cccf4d5b1280cb956
VaughnVernon <vaughn@forcomprehension.com>
2019-08-24 00:47:16 -0700

    Remove contexts.

--- src/main/java/io/vlingo/schemata/resource/data/UnitData.java
+++ src/main/java/io/vlingo/schemata/resource/data/UnitData.java
@@ -7,9 +7,6 @@
 
 package io.vlingo.schemata.resource.data;
 
-import java.util.Collections;
-import java.util.List;
-
 import io.vlingo.schemata.model.UnitState;
 
 public class UnitData {
@@ -17,35 +14,28 @@ public class UnitData {
   public final String unitId;
   public final String name;
   public final String description;
-  public final List<ContextData> contexts;
 
   public static UnitData from(final UnitState state) {
-    return new UnitData(state.unitId.organizationId.value, state.unitId.value, state.name, state.description, Collections.emptyList());
+    return new UnitData(state.unitId.organizationId.value, state.unitId.value, state.name, state.description);
   }
 
   public static UnitData from(final String organizationId, final String unitId, final String name, final String description) {
-    return new UnitData(organizationId, unitId, name, description, Collections.emptyList());
-  }
-
-  public static UnitData from(final String organizationId, final String unitId, final String name, final String description, final List<ContextData> contexts) {
-    return new UnitData(organizationId, unitId, name, description, contexts);
+    return new UnitData(organizationId, unitId, name, description);
   }
 
   public static UnitData just(final String name, final String description) {
-    return new UnitData("", "", name, description, Collections.emptyList());
+    return new UnitData("", "", name, description);
   }
 
   @Override
   public String toString() {
-    return "UnitData [organizationId=" + organizationId + " unitId=" + unitId + " name=" + name + " description="
-            + description + " contexts=" + contexts + "]";
+    return "UnitData [organizationId=" + organizationId + " unitId=" + unitId + " name=" + name + " description=" + description + "]";
   }
 
-  private UnitData(final String organizationId, final String unitId, final String name, final String description, final List<ContextData> contexts) {
+  private UnitData(final String organizationId, final String unitId, final String name, final String description) {
     this.organizationId = organizationId;
     this.unitId = unitId;
     this.name = name;
     this.description = description;
-    this.contexts = contexts;
   }
 }
