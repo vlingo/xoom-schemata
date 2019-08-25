7127f37718df844f76af4d12ed051bd5ef621658
VaughnVernon <vaughn@forcomprehension.com>
2019-08-25 00:18:33 -0700

    Clean up; disabled constraints due to errors.

--- src/main/java/io/vlingo/schemata/infra/persistence/SchemataObjectStore.java
+++ src/main/java/io/vlingo/schemata/infra/persistence/SchemataObjectStore.java
@@ -204,66 +204,66 @@ public class SchemataObjectStore {
         jdbi.handle().execute(
                 "CREATE TABLE IF NOT EXISTS TBL_ORGANIZATIONS (" +
                 "id BIGINT GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) PRIMARY KEY, " +
-                "organizationId VARCHAR (50), " +
-                "name VARCHAR(128), " +
-                "description VARCHAR(8000), " +
+                "organizationId VARCHAR (50) NOT NULL, " +
+                "name VARCHAR(128) NOT NULL, " +
+                "description VARCHAR(8000) " +
 
-                "UNIQUE (name) " +
+//                "UNIQUE (name) " +
                 ")");
 
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS ORG_ALL_INDEX ON TBL_ORGANIZATIONS (organizationId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS ORG_ALL_INDEX ON TBL_ORGANIZATIONS (organizationId)");
     }
 
     private void createUnitStateTable() {
         jdbi.handle().execute(
                 "CREATE TABLE IF NOT EXISTS TBL_UNITS (" +
                 "id BIGINT GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) PRIMARY KEY, " +
-                "unitId VARCHAR(50), " +
-                "organizationId VARCHAR (50), " +
-                "name VARCHAR(128), " +
-                "description VARCHAR(8000), " +
+                "unitId VARCHAR(50) NOT NULL, " +
+                "organizationId VARCHAR (50) NOT NULL, " +
+                "name VARCHAR(128) NOT NULL, " +
+                "description VARCHAR(8000) " +
 
-                "UNIQUE (organizationId, name) " +
+//                "UNIQUE (organizationId, name) " +
                 ")");
 
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS UNIT_PARENT_INDEX ON TBL_UNITS (organizationId)");
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS UNIT_ALL_INDEX ON TBL_UNITS (organizationId, unitId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS UNIT_PARENT_INDEX ON TBL_UNITS (organizationId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS UNIT_ALL_INDEX ON TBL_UNITS (organizationId, unitId)");
     }
 
     private void createContextStateTable() {
         jdbi.handle().execute(
                 "CREATE TABLE IF NOT EXISTS TBL_CONTEXTS (" +
                 "id BIGINT GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) PRIMARY KEY, " +
-                "contextId VARCHAR(50), " +
-                "unitId VARCHAR(50), " +
-                "organizationId VARCHAR (50), " +
-                "namespace VARCHAR(256), " +
-                "description VARCHAR(8000), " +
+                "contextId VARCHAR(50) NOT NULL, " +
+                "unitId VARCHAR(50) NOT NULL, " +
+                "organizationId VARCHAR (50) NOT NULL, " +
+                "namespace VARCHAR(256) NOT NULL, " +
+                "description VARCHAR(8000) " +
 
-                "UNIQUE (unitId, namespace) " +
+//                "UNIQUE (unitId, namespace) " +
                 ")");
 
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS CONTEXT_PARENT_INDEX ON TBL_CONTEXTS (organizationId, unitId)");
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS CONTEXT_ALL_INDEX ON TBL_CONTEXTS (organizationId, unitId, contextId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS CONTEXT_PARENT_INDEX ON TBL_CONTEXTS (organizationId, unitId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS CONTEXT_ALL_INDEX ON TBL_CONTEXTS (organizationId, unitId, contextId)");
     }
 
     private void createSchemaStateTable() {
         jdbi.handle().execute(
                 "CREATE TABLE IF NOT EXISTS TBL_SCHEMAS (" +
                 "id BIGINT GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) PRIMARY KEY, " +
-                "schemaId VARCHAR(50), " +
-                "contextId VARCHAR(50), " +
-                "unitId VARCHAR(50), " +
-                "organizationId VARCHAR (50), " +
-                "category VARCHAR(25), " +
-                "name VARCHAR(128), " +
-                "description VARCHAR(8000), " +
+                "schemaId VARCHAR(50) NOT NULL, " +
+                "contextId VARCHAR(50) NOT NULL, " +
+                "unitId VARCHAR(50) NOT NULL, " +
+                "organizationId VARCHAR (50) NOT NULL, " +
+                "category VARCHAR(25) NOT NULL, " +
+                "name VARCHAR(128) NOT NULL, " +
+                "description VARCHAR(8000) " +
 
-                "UNIQUE (contextId, category, name) " +
+//                "UNIQUE (contextId, category, name) " +
                 ")");
 
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMA_PARENT_INDEX ON TBL_SCHEMAS (organizationId, unitId, contextId)");
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMA_ALL_INDEX ON TBL_SCHEMAS (organizationId, unitId, contextId, schemaId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMA_PARENT_INDEX ON TBL_SCHEMAS (organizationId, unitId, contextId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMA_ALL_INDEX ON TBL_SCHEMAS (organizationId, unitId, contextId, schemaId)");
     }
 
     private void createSchemaVersionStateTable() {
@@ -284,21 +284,21 @@ public class SchemataObjectStore {
         jdbi.handle().execute(
                 "CREATE TABLE IF NOT EXISTS TBL_SCHEMAVERSIONS (" +
                 "id BIGINT GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) PRIMARY KEY, " +
-                "schemaVersionId VARCHAR(50), " +
-                "schemaId VARCHAR(50), " +
-                "contextId VARCHAR(50), " +
-                "unitId VARCHAR(50), " +
-                "organizationId VARCHAR (50), " +
-                "specification VARCHAR(" + specificationWidth + "), " +
+                "schemaVersionId VARCHAR(50) NOT NULL, " +
+                "schemaId VARCHAR(50) NOT NULL, " +
+                "contextId VARCHAR(50) NOT NULL, " +
+                "unitId VARCHAR(50) NOT NULL, " +
+                "organizationId VARCHAR (50) NOT NULL, " +
+                "specification VARCHAR(" + specificationWidth + ") NOT NULL, " +
                 "description VARCHAR(8000), " +
-                "status VARCHAR(16), " +
-                "previousVersion VARCHAR(20), " +
-                "currentVersion VARCHAR(20), " +
+                "status VARCHAR(16) NOT NULL, " +
+                "previousVersion VARCHAR(20) NOT NULL, " +
+                "currentVersion VARCHAR(20) NOT NULL " +
 
-                "UNIQUE (schemaId, currentVersion) " +
+//                "UNIQUE (schemaId, currentVersion) " +
                 ")");
 
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMAVERSION_PARENT_INDEX ON TBL_SCHEMAVERSIONS (organizationId, unitId, contextId, schemaId)");
-        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMAVERSION_ALL_INDEX ON TBL_SCHEMAVERSIONS (organizationId, unitId, contextId, schemaId, schemaVersionId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMAVERSION_PARENT_INDEX ON TBL_SCHEMAVERSIONS (organizationId, unitId, contextId, schemaId)");
+//        jdbi.handle().execute("CREATE UNIQUE INDEX IF NOT EXISTS SCHEMAVERSION_ALL_INDEX ON TBL_SCHEMAVERSIONS (organizationId, unitId, contextId, schemaId, schemaVersionId)");
     }
 }
