1e80723158b8e2ffd436dbe9c27a02a9e9e5d65a
VaughnVernon <vaughn@forcomprehension.com>
2019-08-27 02:11:26 -0700

    Full implementation.

--- src/main/java/io/vlingo/schemata/query/SchemaQueriesActor.java
+++ src/main/java/io/vlingo/schemata/query/SchemaQueriesActor.java
@@ -7,27 +7,63 @@
 
 package io.vlingo.schemata.query;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import io.vlingo.actors.Actor;
 import io.vlingo.common.Completes;
+import io.vlingo.lattice.query.StateObjectQueryActor;
+import io.vlingo.schemata.model.SchemaState;
 import io.vlingo.schemata.resource.data.SchemaData;
-import io.vlingo.symbio.store.object.jdbc.jdbi.JdbiOnDatabase;
+import io.vlingo.symbio.store.object.MapQueryExpression;
+import io.vlingo.symbio.store.object.ObjectStore;
+import io.vlingo.symbio.store.object.QueryExpression;
 
-public class SchemaQueriesActor extends Actor implements SchemaQueries {
-  private final JdbiOnDatabase jdbi;
+public class SchemaQueriesActor extends StateObjectQueryActor implements SchemaQueries {
+  private final Map<String,String> parameters;
 
-  public SchemaQueriesActor(final JdbiOnDatabase jdbi) {
-    this.jdbi = jdbi;
+  public SchemaQueriesActor(final ObjectStore objectStore) {
+    super(objectStore);
+    this.parameters = new HashMap<>(4);
   }
 
   @Override
   public Completes<List<SchemaData>> schemas(final String organizationId, final String unitId, final String contextId) {
-    return null;
+    parameters.clear();
+    parameters.put("organizationId", organizationId);
+    parameters.put("unitId", unitId);
+    parameters.put("contextId", contextId);
+
+    final QueryExpression query =
+            MapQueryExpression.using(
+                    SchemaState.class,
+                    "SELECT * FROM TBL_SCHEMAS " +
+                    "WHERE organizationId = :organizationId " +
+                      "AND unitId = :unitId " +
+                      "AND contextId = :contextId",
+                    parameters);
+
+    return queryAll(SchemaState.class, query, (List<SchemaState> states) -> SchemaData.from(states));
   }
 
   @Override
   public Completes<SchemaData> schema(final String organizationId, final String unitId, final String contextId, final String schemaId) {
-    return null;
+    parameters.clear();
+    parameters.put("organizationId", organizationId);
+    parameters.put("unitId", unitId);
+    parameters.put("contextId", contextId);
+    parameters.put("schemaId", schemaId);
+
+    final QueryExpression query =
+            MapQueryExpression.using(
+                    SchemaState.class,
+                    "SELECT * FROM TBL_SCHEMAS " +
+                    "WHERE organizationId = :organizationId " +
+                      "AND unitId = :unitId " +
+                      "AND contextId = :contextId " +
+                      "AND schemaId = :schemaId",
+                    parameters);
+
+    return queryObject(SchemaState.class, query, (SchemaState state) -> SchemaData.from(state));
   }
 }
