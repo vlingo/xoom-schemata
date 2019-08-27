0fa6f9d2771c65b657e25b6b44763736d516a8df
VaughnVernon <vaughn@forcomprehension.com>
2019-08-27 02:17:04 -0700

    Initialize query with ObjectStore.

--- src/test/java/io/vlingo/schemata/resource/SchemaResourceTest.java
+++ src/test/java/io/vlingo/schemata/resource/SchemaResourceTest.java
@@ -109,9 +109,10 @@ public class SchemaResourceTest extends ResourceTest {
   public void setUp() throws Exception {
     world = World.startWithDefaults("test-command-router");
     world.stageNamed("vlingo-schemata-grid", Grid.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
-    queries = Queries.forSchemas(world.stageNamed(Schemata.StageName), jdbi());
 
     objectStore = world.actorFor(ObjectStore.class, InMemoryObjectStoreActor.class, new NoopDispatcher());
+
+    queries = Queries.forSchemas(world.stageNamed(Schemata.StageName), objectStore);
 
     registry = new ObjectTypeRegistry(world);
 
