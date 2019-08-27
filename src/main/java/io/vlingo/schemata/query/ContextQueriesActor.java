2950ad3467a4a77c867ab2d52dc367ed7a106ead
VaughnVernon <vaughn@forcomprehension.com>
2019-08-27 02:10:56 -0700

    Full implementation.

--- src/main/java/io/vlingo/schemata/query/ContextQueriesActor.java
+++ src/main/java/io/vlingo/schemata/query/ContextQueriesActor.java
@@ -7,27 +7,59 @@
 
 package io.vlingo.schemata.query;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import io.vlingo.actors.Actor;
 import io.vlingo.common.Completes;
-import io.vlingo.schemata.resource.data.UnitData;
-import io.vlingo.symbio.store.object.jdbc.jdbi.JdbiOnDatabase;
+import io.vlingo.lattice.query.StateObjectQueryActor;
+import io.vlingo.schemata.model.ContextState;
+import io.vlingo.schemata.resource.data.ContextData;
+import io.vlingo.symbio.store.object.MapQueryExpression;
+import io.vlingo.symbio.store.object.ObjectStore;
+import io.vlingo.symbio.store.object.QueryExpression;
 
-public class ContextQueriesActor extends Actor implements UnitQueries {
-  private final JdbiOnDatabase jdbi;
+public class ContextQueriesActor extends StateObjectQueryActor implements ContextQueries {
+  private final Map<String,String> parameters;
 
-  public ContextQueriesActor(final JdbiOnDatabase jdbi) {
-    this.jdbi = jdbi;
+  public ContextQueriesActor(final ObjectStore objectStore) {
+    super(objectStore);
+    this.parameters = new HashMap<>(3);
   }
 
   @Override
-  public Completes<List<UnitData>> units(final String organizationId) {
-    return null;
+  public Completes<List<ContextData>> contexts(final String organizationId, final String unitId) {
+    parameters.clear();
+    parameters.put("organizationId", organizationId);
+    parameters.put("unitId", unitId);
+
+    final QueryExpression query =
+            MapQueryExpression.using(
+                    ContextState.class,
+                    "SELECT * FROM TBL_CONTEXTS " +
+                    "WHERE organizationId = :organizationId " +
+                      "AND unitId = :unitId",
+                    parameters);
+
+    return queryAll(ContextState.class, query, (List<ContextState> states) -> ContextData.from(states));
   }
 
   @Override
-  public Completes<UnitData> unit(final String organizationId, final String unitId) {
-    return null;
+  public Completes<ContextData> context(final String organizationId, final String unitId, final String contextId) {
+    parameters.clear();
+    parameters.put("organizationId", organizationId);
+    parameters.put("unitId", unitId);
+    parameters.put("contextId", contextId);
+
+    final QueryExpression query =
+            MapQueryExpression.using(
+                    ContextState.class,
+                    "SELECT * FROM TBL_CONTEXTS " +
+                    "WHERE organizationId = :organizationId " +
+                      "AND unitId = :unitId " +
+                      "AND contextId = :contextId",
+                    parameters);
+
+    return queryObject(ContextState.class, query, (ContextState state) -> ContextData.from(state));
   }
 }
