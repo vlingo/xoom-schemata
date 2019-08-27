871268fb0b17c21e30d2dc1a6777cad0d6440022
VaughnVernon <vaughn@forcomprehension.com>
2019-08-27 02:11:10 -0700

    Full implementation.

--- src/main/java/io/vlingo/schemata/query/OrganizationQueriesActor.java
+++ src/main/java/io/vlingo/schemata/query/OrganizationQueriesActor.java
@@ -7,27 +7,47 @@
 
 package io.vlingo.schemata.query;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import io.vlingo.actors.Actor;
 import io.vlingo.common.Completes;
+import io.vlingo.lattice.query.StateObjectQueryActor;
+import io.vlingo.schemata.model.OrganizationState;
 import io.vlingo.schemata.resource.data.OrganizationData;
-import io.vlingo.symbio.store.object.jdbc.jdbi.JdbiOnDatabase;
+import io.vlingo.symbio.store.object.MapQueryExpression;
+import io.vlingo.symbio.store.object.ObjectStore;
+import io.vlingo.symbio.store.object.QueryExpression;
 
-public class OrganizationQueriesActor extends Actor implements OrganizationQueries {
-  private final JdbiOnDatabase jdbi;
+public class OrganizationQueriesActor extends StateObjectQueryActor implements OrganizationQueries {
+  private final Map<String,String> parameters;
+  private final QueryExpression queryAll;
 
-  public OrganizationQueriesActor(final JdbiOnDatabase jdbi) {
-    this.jdbi = jdbi;
+  public OrganizationQueriesActor(final ObjectStore objectStore) {
+    super(objectStore);
+
+    this.parameters = new HashMap<>(1);
+
+    this.queryAll = QueryExpression.using(OrganizationState.class, "SELECT * FROM TBL_ORGANIZATIONS");
   }
 
   @Override
   public Completes<List<OrganizationData>> organizations() {
-    return null;
+    return queryAll(OrganizationState.class, queryAll, (List<OrganizationState> states) -> OrganizationData.from(states));
   }
 
   @Override
   public Completes<OrganizationData> organization(final String organizationId) {
-    return null;
+    parameters.clear();
+    parameters.put("organizationId", organizationId);
+
+    final QueryExpression query =
+            MapQueryExpression.using(
+                    OrganizationState.class,
+                    "SELECT * FROM TBL_ORGANIZATIONS " +
+                    "WHERE organizationId = :organizationId",
+                    parameters);
+
+    return queryObject(OrganizationState.class, query, (OrganizationState state) -> OrganizationData.from(state));
   }
 }
