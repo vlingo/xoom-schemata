ddd953089c4946ea558f97c2bcd9a22a6196bdf2
VaughnVernon <vaughn@forcomprehension.com>
2019-09-09 00:41:49 -0700

    Initial revision of PathData.

---
+++ src/main/java/io/vlingo/schemata/resource/data/PathData.java
@@ -0,0 +1,32 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.schemata.resource.data;
+
+public class PathData {
+  public final String organization;
+  public final String unit;
+  public final String context;
+  public final String schema;
+  public final String version;
+
+  public static PathData from(final String reference) {
+    return new PathData(reference);
+  }
+
+  public PathData(final String reference) {
+    final String[] parts = reference.split(":");
+
+    assert parts.length == 5 : "The reference path must have five parts: org:unit:context:schema:version";
+
+    this.organization = parts[0];
+    this.unit = parts[1];
+    this.context = parts[2];
+    this.schema = parts[3];
+    this.version = parts[4];
+  }
+}
