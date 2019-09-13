c5290657f0c8885ac75449030d3bb07ef9ff36bb
Wolfgang Werner <mail@wolfgang-werner.net>
2019-09-13 13:06:28 +0200

    [WIP] Upgrade vuetify to v2.0.17

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-app-bar>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vlingo</span>
         <span class="font-weight-light">Schemata</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-
-    </v-toolbar>
+    </v-app-bar>
 
     <v-content>
       <router-view/>
--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,13 +1,15 @@
 import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
+import vuetify from '@/plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
