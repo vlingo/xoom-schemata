08f36eec4f7899d23ada1f2c09a6ad00b189e16f
VaughnVernon <vaughn@forcomprehension.com>
2019-09-18 16:22:02 -0700

    Enriched with fullyQualifiedTypeName.

--- src/main/java/io/vlingo/schemata/codegen/ast/types/TypeDefinition.java
+++ src/main/java/io/vlingo/schemata/codegen/ast/types/TypeDefinition.java
@@ -1,17 +1,26 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata.codegen.ast.types;
+
+import java.util.List;
 
 import io.vlingo.schemata.codegen.ast.Node;
 import io.vlingo.schemata.model.Category;
 
-import java.util.List;
-
 public class TypeDefinition implements Node, Type {
     public final Category category;
+    public final String fullyQualifiedTypeName;
     public final String typeName;
     public final List<Node> children;
 
-    public TypeDefinition(Category category, String typeName, List<Node> children) {
+    public TypeDefinition(Category category, final String fullyQualifiedTypeName, String typeName, List<Node> children) {
         this.category = category;
+        this.fullyQualifiedTypeName = fullyQualifiedTypeName;
         this.typeName = typeName;
         this.children = children;
     }
