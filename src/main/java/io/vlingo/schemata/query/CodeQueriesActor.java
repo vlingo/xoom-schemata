70d899128374286ad5138386d67c22bc6ba992bb
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-10-16 23:03:12 +0200

    Code Queries Actors refactor to one query instead of individual ones

--- src/main/java/io/vlingo/schemata/query/CodeQueriesActor.java
+++ src/main/java/io/vlingo/schemata/query/CodeQueriesActor.java
@@ -7,51 +7,75 @@
 
 package io.vlingo.schemata.query;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-import io.vlingo.actors.Actor;
 import io.vlingo.actors.CompletesEventually;
 import io.vlingo.common.Completes;
 import io.vlingo.common.version.SemanticVersion;
-import io.vlingo.schemata.resource.data.AuthorizationData;
-import io.vlingo.schemata.resource.data.OrganizationData;
-import io.vlingo.schemata.resource.data.PathData;
-import io.vlingo.schemata.resource.data.SchemaVersionData;
+import io.vlingo.lattice.query.StateObjectQueryActor;
+import io.vlingo.schemata.model.SchemaVersionState;
+import io.vlingo.schemata.resource.data.*;
+import io.vlingo.symbio.store.object.MapQueryExpression;
+import io.vlingo.symbio.store.object.ObjectStore;
+import io.vlingo.symbio.store.object.QueryExpression;
 
-public class CodeQueriesActor extends Actor implements CodeQueries {
-  private final OrganizationQueries organizationQueries;
-  private final UnitQueries unitQueries;
-  private final ContextQueries contextQueries;
-  private final SchemaQueries schemaQueries;
-  private final SchemaVersionQueries schemaVersionQueries;
+public class CodeQueriesActor extends StateObjectQueryActor implements CodeQueries {
 
-  public CodeQueriesActor(
-          final OrganizationQueries organizationQueries,
-          final UnitQueries unitQueries,
-          final ContextQueries contextQueries,
-          final SchemaQueries schemaQueries,
-          final SchemaVersionQueries schemaVersionQueries) {
+  private static final String SchemaVersion =
+          "SELECT TBL_SCHEMAVERSIONS.schemaVersionId, " +
+                  "TBL_SCHEMAVERSIONS.schemaId, " +
+                  "TBL_SCHEMAVERSIONS.contextId, " +
+                  "TBL_SCHEMAVERSIONS.unitId, " +
+                  "TBL_SCHEMAVERSIONS.organizationId, " +
+                  "TBL_SCHEMAVERSIONS.description, " +
+                  "TBL_SCHEMAVERSIONS.specification, " +
+                  "TBL_SCHEMAVERSIONS.status, " +
+                  "TBL_SCHEMAVERSIONS.previousVersion, " +
+                  "TBL_SCHEMAVERSIONS.currentVersion " +
+              "FROM TBL_SCHEMAVERSIONS " +
+              "INNER JOIN TBL_SCHEMAS ON TBL_SCHEMAVERSIONS.schemaId = TBL_SCHEMAS.schemaId " +
+                  "AND TBL_SCHEMAVERSIONS.organizationId = TBL_SCHEMAS.organizationId " +
+                  "AND TBL_SCHEMAVERSIONS.unitId = TBL_SCHEMAS.unitId " +
+                  "AND TBL_SCHEMAVERSIONS.contextId = TBL_SCHEMAS.contextId " +
+              "INNER JOIN TBL_CONTEXTS ON TBL_CONTEXTS.contextId = TBL_SCHEMAVERSIONS.contextId " +
+                  "AND TBL_SCHEMAVERSIONS.organizationId = TBL_CONTEXTS.organizationId " +
+                  "AND TBL_SCHEMAVERSIONS.unitId = TBL_CONTEXTS.unitId " +
+              "INNER JOIN TBL_UNITS ON TBL_UNITS.unitId = TBL_SCHEMAVERSIONS.unitId " +
+                  "AND TBL_SCHEMAVERSIONS.organizationId = TBL_UNITS.organizationId " +
+              "INNER JOIN TBL_ORGANIZATIONS ON TBL_ORGANIZATIONS.organizationId = TBL_SCHEMAVERSIONS.organizationId " +
+              "WHERE TBL_SCHEMAVERSIONS.schemaVersionId = :schemaVersionId " +
+                  "AND TBL_CONTEXTS.namespace = :namespace " +
+                  "AND TBL_UNITS.name = :unitName " +
+                  "AND TBL_SCHEMAS.name = :schemaName " +
+                  "AND TBL_ORGANIZATIONS.name = :organizationName";
+  private final Map<String,String> parameters;
 
-    this.organizationQueries = organizationQueries;
-    this.unitQueries = unitQueries;
-    this.contextQueries = contextQueries;
-    this.schemaQueries = schemaQueries;
-    this.schemaVersionQueries = schemaVersionQueries;
+  public CodeQueriesActor(final ObjectStore objectStore) {
+    super(objectStore);
+
+    this.parameters = new HashMap<>(5);
   }
 
   @Override
   public Completes<SchemaVersionData> schemaVersionFor(final AuthorizationData authorization, final PathData path) {
     final CompletesEventually completesEventually = completesEventually();
 
-    organizationQueries.organizationNamed(path.organization)
-      .andThenTo(organization -> validate(authorization, organization))
-      .andThenTo(organization -> unitQueries.unitNamed(organization.organizationId, path.unit))
-      .andThenTo(unit -> contextQueries.contextOfNamespace(unit.organizationId, unit.unitId, path.context))
-      .andThenTo(context -> schemaQueries.schemaNamed(context.organizationId, context.unitId, context.contextId, path.schema))
-      .andThenTo(schema -> schemaVersionQueries.schemaVersionOfVersion(schema.organizationId, schema.unitId, schema.contextId, schema.schemaId, path.version))
-      .andThenTo(schemaVersion -> { completesEventually.with(schemaVersion); return Completes.withSuccess(schemaVersion); })
-      .otherwise(failure -> SchemaVersionData.none())
-      .recoverFrom(exception -> SchemaVersionData.none());
+    parameters.clear();
+    parameters.put("schemaVersionId", path.version);
+    parameters.put("namespace", path.context);
+    parameters.put("unitName", path.unit);
+    parameters.put("schemaName", path.schema);
+    parameters.put("organizationName", path.organization);
+
+    final QueryExpression query = MapQueryExpression.using(SchemaVersionState.class, SchemaVersion, parameters);
+
+    queryObject(SchemaVersionState.class, query, (SchemaVersionState state) -> SchemaVersionData.from(state))
+          .andThenTo(schemaVersion -> validate(authorization, schemaVersion))
+          .andThenTo(schemaVersion -> { completesEventually.with(schemaVersion); return Completes.withSuccess(schemaVersion); })
+          .otherwise(failure -> SchemaVersionData.none())
+          .recoverFrom(exception -> SchemaVersionData.none());
 
     return completes();
   }
@@ -70,22 +94,27 @@ public class CodeQueriesActor extends Actor implements CodeQueries {
 
       final String version = path.versionOrElse(SemanticVersion.greatest().toString());
 
-      organizationQueries.organizationNamed(path.organization)
-        .andThenTo(organization -> unitQueries.unitNamed(organization.organizationId, path.unit))
-        .andThenTo(unit -> contextQueries.contextOfNamespace(unit.organizationId, unit.unitId, path.context))
-        .andThenTo(context -> schemaQueries.schemaNamed(context.organizationId, context.unitId, context.contextId, path.schema))
-        .andThenTo(schema -> schemaVersionQueries.schemaVersionOfVersion(schema.organizationId, schema.unitId, schema.contextId, schema.schemaId, version))
-        .andThenTo(schemaVersion -> { completesEventually.with(schemaVersion); return Completes.withSuccess(schemaVersion); })
-        .otherwise(failure -> SchemaVersionData.none())
-        .recoverFrom(exception -> SchemaVersionData.none());
+      parameters.clear();
+      parameters.put("schemaVersionId", version);
+      parameters.put("namespace", path.context);
+      parameters.put("unitName", path.unit);
+      parameters.put("schemaName", path.schema);
+      parameters.put("organizationName", path.organization);
+
+      final QueryExpression query = MapQueryExpression.using(SchemaVersionState.class, SchemaVersion, parameters);
+
+      queryObject(SchemaVersionState.class, query, (SchemaVersionState state) -> SchemaVersionData.from(state))
+              .andThenTo(schemaVersion -> { completesEventually.with(schemaVersion); return Completes.withSuccess(schemaVersion); })
+              .otherwise(failure -> SchemaVersionData.none())
+              .recoverFrom(exception -> SchemaVersionData.none());
     });
 
     return completes();
   }
 
-  private Completes<OrganizationData> validate(final AuthorizationData authorization, final OrganizationData organization) {
-    return authorization.sameSource(organization.organizationId) ?
-            Completes.withSuccess(organization) :
+  private Completes<SchemaVersionData> validate(final AuthorizationData authorization, final SchemaVersionData schemaVersionData) {
+    return authorization.sameSource(schemaVersionData.organizationId) ?
+            Completes.withSuccess(schemaVersionData) :
             Completes.withFailure();
   }
 
