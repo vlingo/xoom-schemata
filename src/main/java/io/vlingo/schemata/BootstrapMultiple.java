58a13667848759996b0d98779f1b85642a9af17c
wwerner <mail@wolfgang-werner.net>
2019-11-15 12:29:44 +0100

    Remove unused imports

--- src/main/java/io/vlingo/schemata/BootstrapMultiple.java
+++ src/main/java/io/vlingo/schemata/BootstrapMultiple.java
@@ -7,24 +7,14 @@
 
 package io.vlingo.schemata;
 
-import io.vlingo.actors.Stage;
 import io.vlingo.actors.World;
-import io.vlingo.common.Completes;
 import io.vlingo.common.identity.IdentityGeneratorType;
-import io.vlingo.http.Body;
-import io.vlingo.http.Response;
-import io.vlingo.http.resource.*;
+import io.vlingo.http.resource.Configuration;
+import io.vlingo.http.resource.Resources;
+import io.vlingo.http.resource.Server;
 import io.vlingo.lattice.grid.Grid;
 import io.vlingo.lattice.grid.GridAddressFactory;
-import io.vlingo.lattice.model.object.ObjectTypeRegistry;
-import io.vlingo.schemata.infra.persistence.SchemataObjectStore;
-import io.vlingo.schemata.query.Queries;
-import io.vlingo.schemata.resource.*;
-import io.vlingo.symbio.BaseEntry.TextEntry;
-import io.vlingo.symbio.State.TextState;
-import io.vlingo.symbio.store.object.ObjectStore;
 
-import static io.vlingo.http.Response.Status.Ok;
 import static io.vlingo.schemata.Schemata.StageName;
 
 public class BootstrapMultiple {
@@ -37,25 +27,25 @@ public class BootstrapMultiple {
 
   /**
    * Demonstrates that running two servers simultaneously leads to hanging requests on both.
-   *
+   * <p>
    * To reproduce:
    * * Run this class
    * * Try to access http://localhost:9019 or http://localhost:9020
-   *
+   * <p>
    * Once you remove one of the two servers, requests on the remaining one finish as expected
    */
   public BootstrapMultiple() throws Exception {
     world = World.startWithDefaults("test-world");
     world.stageNamed(StageName, Grid.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
 
-     server1 = Server.startWith(world.stage(),
-       Resources.are(new TestResource(world,"foo").routes()),
+    server1 = Server.startWith(world.stage(),
+      Resources.are(new TestResource(world, "foo").routes()),
       9019,
       Configuration.Sizing.define(),
       Configuration.Timing.define());
 
-     server2 = Server.startWith(world.stage(),
-       Resources.are(new TestResource(world,"bar").routes()),
+    server2 = Server.startWith(world.stage(),
+      Resources.are(new TestResource(world, "bar").routes()),
       9020,
       Configuration.Sizing.define(),
       Configuration.Timing.define());
