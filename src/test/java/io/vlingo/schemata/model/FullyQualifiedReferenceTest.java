a4552cd26e4fde9631847a4973c0ccb14401d3c1
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-07 12:09:08 +0100

    Add reference model to keep parsing reference strings in one place

---
+++ src/test/java/io/vlingo/schemata/model/FullyQualifiedReferenceTest.java
@@ -0,0 +1,54 @@
+package io.vlingo.schemata.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FullyQualifiedReferenceTest {
+
+    @Test
+    public void schemaReferenceCanBeProducedFromReferenceString() {
+        String refStr = "org:unit:context:schema";
+        FullyQualifiedReference ref = FullyQualifiedReference.from(refStr);
+
+        assertEquals("org", ref.organization);
+        assertEquals("unit", ref.unit);
+        assertEquals("context", ref.context);
+        assertEquals("schema", ref.schema);
+
+        assertFalse(ref.isSchemaVersionReference());
+    }
+
+    @Test
+    public void schemaVersionReferenceCanBeProducedFromReferenceString() {
+        String refStr = "org:unit:context:schema:0.1.1";
+        FullyQualifiedReference ref = FullyQualifiedReference.from(refStr);
+
+        assertEquals("org", ref.organization);
+        assertEquals("unit", ref.unit);
+        assertEquals("context", ref.context);
+        assertEquals("schema", ref.schema);
+        assertEquals("0.1.1", ref.schemaVersion);
+
+        assertTrue(ref.isSchemaVersionReference());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void incompleteReferencesCannotBeCreated() {
+        String refStr = "org:unit:context";
+        FullyQualifiedReference.from(refStr);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void referencesWithEmptyPartsCannotBeCreated() {
+        String refStr = "org:unit::schema";
+        FullyQualifiedReference.from(refStr);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void referencesWithTooManyPartsCannotBeCreated() {
+        String refStr = "org:unit:context:schema:0.0.0:invalid";
+        FullyQualifiedReference.from(refStr);
+    }
+
+}
