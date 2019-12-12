vlingo/xoom-schemata

commit 51826d0020af0df6c676b44f20245509050f43c7

Update test w/ duplicate schema names in reference

src/main/java/io/vlingo/schemata/codegen/backend/java/JavaBackend.java
@@ -141,10 +141,11 @@ public class JavaBackend extends Actor implements Backend {
 
     private String packageOf(String category, String reference) {
         String[] referenceParts = reference.split(Schemata.ReferenceSeparator);
-        if (referenceParts.length != 4) {
-            throw new IllegalArgumentException("Invalid fully qualified type name. Valid type names look like this <organization>:<unit>:<context namespace>:<type name>.");
+        if (!(referenceParts.length >= Schemata.MinReferenceParts && referenceParts.length <= Schemata.MaxReferenceParts))
+        {
+            throw new IllegalArgumentException(String.format("Invalid fully qualified type name %s. Valid type names look like this <organization>:<unit>:<context namespace>:<type name>.", reference));
         }
-        final String namespace = referenceParts[2];
+        final String namespace = referenceParts[2].toLowerCase();
         final String className = referenceParts[3];
 
         final String basePackage = namespace + "." + category;

src/test/java/io/vlingo/schemata/codegen/specs/JavaCodeGenTests.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertTrue;
 public class JavaCodeGenTests extends CodeGenTests {
   @Test
   public void testThatGeneratesABasicType() throws ExecutionException, InterruptedException {
-    final String fullyQualifiedTypeName = "Org:Unit:Context:Schema:SalutationHappened";
+    final String fullyQualifiedTypeName = "Org:Unit:Context:SalutationHappened";
 
     final String result = compilerWithJavaBackend().compile(typeDefinition("basic"), fullyQualifiedTypeName, "0.0.1").await(TIMEOUT);
 
@@ -29,7 +29,7 @@ public class JavaCodeGenTests extends CodeGenTests {
     assertTrue(result.contains("public final SchemaVersion.Version eventVersion;"));
     assertTrue(result.contains("public final String toWhom;"));
     assertTrue(result.contains("public final String text;"));
-    assertTrue(result.contains("public final SalutationHappened(final String toWhom, final String text) {"));
+    assertTrue(result.contains("public SalutationHappened(final String toWhom, final String text) {"));
     assertTrue(result.contains("this.eventType = \"SalutationHappened\";"));
     assertTrue(result.contains("this.occurredOn = System.currentTimeMillis();"));
     assertTrue(result.contains("this.eventVersion = SchemaVersion.Version.of(\"0.0.1\");"));
@@ -39,7 +39,7 @@ public class JavaCodeGenTests extends CodeGenTests {
 
   @Test
   public void testThatGeneratesABasicTypeWithAllConsideredInnerTypes() throws ExecutionException, InterruptedException {
-    final String fullyQualifiedTypeName = "Org:Unit:Context:Schema:SalutationHappened";
+    final String fullyQualifiedTypeName = "Org:Unit:Context:SalutationHappened";
 
     final String result = compilerWithJavaBackend().compile(typeDefinition("allSingleTypes"), fullyQualifiedTypeName, "0.0.1").await(TIMEOUT);
 
@@ -56,15 +56,15 @@ public class JavaCodeGenTests extends CodeGenTests {
 
   @Test
   public void testThatGeneratesAComposedTypeWithVersionedData() throws ExecutionException, InterruptedException {
-    registerType("types/price", "Org:Unit:Context:Schema:Price", "1.0.0");
-    final String result = compilerWithJavaBackend().compile(typeDefinition("price-changed"), "Org:Unit:Context:Schema:PriceChanged", "0.5.1").await();
+    registerType("types/price", "Org:Unit:Context:Price", "1.0.0");
+    final String result = compilerWithJavaBackend().compile(typeDefinition("price-changed"), "Org:Unit:Context:PriceChanged", "0.5.1").await();
 
     assertTrue(result.contains("public final class PriceChanged extends DomainEvent {"));
     assertTrue(result.contains("public final long occurredOn;"));
     assertTrue(result.contains("public final SchemaVersion.Version eventVersion;"));
     assertTrue(result.contains("public final Price oldPrice;"));
     assertTrue(result.contains("public final Price newPrice;"));
-    assertTrue(result.contains("public final PriceChanged(final Price oldPrice, final Price newPrice) {"));
+    assertTrue(result.contains("public PriceChanged(final Price oldPrice, final Price newPrice) {"));
     assertTrue(result.contains("this.occurredOn = System.currentTimeMillis();"));
     assertTrue(result.contains("this.eventVersion = SchemaVersion.Version.of(\"0.5.1\");"));
     assertTrue(result.contains("this.oldPrice = oldPrice;"));

src/test/java/io/vlingo/schemata/resource/JavaCodeResourceTest.java
@@ -38,13 +38,16 @@ public class JavaCodeResourceTest extends ResourceTest {
     final Response response = resource.queryCodeForLanguage(reference(), "java").await();
     assertEquals(Ok, response.status);
     assertTrue(response.entity.content().contains("SchemaDefined"));
-    assertTrue(response.entity.content().equals(
+    assertEquals(
+            "package io.vlingo.schemata.event;\n" +
+            "\n" +
             "import io.vlingo.lattice.model.DomainEvent;\n" +
             "\n" +
             "public final class SchemaDefined extends DomainEvent {\n" +
-            "  public final SchemaDefined() {\n" +
+            "  public SchemaDefined() {\n" +
             "  }\n" +
-            "}\n"));
+            "}\n",
+            response.entity.content());
   }
 
   @Before

src/main/java/io/vlingo/schemata/Schemata.java
@@ -21,6 +21,7 @@ public class Schemata {
    * (organization, unit, context, schema, [schemaVersion])
    */
   public static final int MinReferenceParts = 4;
+  public static final int MaxReferenceParts = 5;
 
   //==========================
   // Paths

src/test/resources/io/vlingo/schemata/codegen/vss/price-changed.vss
@@ -1,6 +1,6 @@
 event PriceChanged {
     version eventVersion
     timestamp occurredOn
-    Org:Unit:Context:Schema:Price oldPrice
-    Org:Unit:Context:Schema:Price:1.0.0 newPrice
+    Org:Unit:Context:Price oldPrice
+    Org:Unit:Context:Price:1.0.0 newPrice
 }

commit 52f57efa04f04c5b683b79da0aa55ef833d40b11

Update tests in a less invasive manner, 51826d0 broke more than intended

src/main/java/io/vlingo/schemata/codegen/backend/java/JavaBackend.java
@@ -141,8 +141,8 @@ public class JavaBackend extends Actor implements Backend {
 
     private String packageOf(String category, String reference) {
         String[] referenceParts = reference.split(Schemata.ReferenceSeparator);
-        if (referenceParts.length != 4) {
-            throw new IllegalArgumentException("Invalid fully qualified type name. Valid type names look like this <organization>:<unit>:<context namespace>:<type name>.");
+        if (referenceParts.length < Schemata.MinReferenceParts) {
+            throw new IllegalArgumentException("Invalid fully qualified type name. Valid type names look like this <organization>:<unit>:<context namespace>:<type name>[:<version>].");
         }
         final String namespace = referenceParts[2];
         final String className = referenceParts[3];

src/test/java/io/vlingo/schemata/codegen/specs/JavaCodeGenTests.java
@@ -29,7 +29,7 @@ public class JavaCodeGenTests extends CodeGenTests {
     assertTrue(result.contains("public final SchemaVersion.Version eventVersion;"));
     assertTrue(result.contains("public final String toWhom;"));
     assertTrue(result.contains("public final String text;"));
-    assertTrue(result.contains("public final SalutationHappened(final String toWhom, final String text) {"));
+    assertTrue(result.contains("public SalutationHappened(final String toWhom, final String text) {"));
     assertTrue(result.contains("this.eventType = \"SalutationHappened\";"));
     assertTrue(result.contains("this.occurredOn = System.currentTimeMillis();"));
     assertTrue(result.contains("this.eventVersion = SchemaVersion.Version.of(\"0.0.1\");"));
@@ -64,7 +64,7 @@ public class JavaCodeGenTests extends CodeGenTests {
     assertTrue(result.contains("public final SchemaVersion.Version eventVersion;"));
     assertTrue(result.contains("public final Price oldPrice;"));
     assertTrue(result.contains("public final Price newPrice;"));
-    assertTrue(result.contains("public final PriceChanged(final Price oldPrice, final Price newPrice) {"));
+    assertTrue(result.contains("public PriceChanged(final Price oldPrice, final Price newPrice) {"));
     assertTrue(result.contains("this.occurredOn = System.currentTimeMillis();"));
     assertTrue(result.contains("this.eventVersion = SchemaVersion.Version.of(\"0.5.1\");"));
     assertTrue(result.contains("this.oldPrice = oldPrice;"));

src/test/java/io/vlingo/schemata/resource/JavaCodeResourceTest.java
@@ -38,13 +38,15 @@ public class JavaCodeResourceTest extends ResourceTest {
     final Response response = resource.queryCodeForLanguage(reference(), "java").await();
     assertEquals(Ok, response.status);
     assertTrue(response.entity.content().contains("SchemaDefined"));
-    assertTrue(response.entity.content().equals(
+    assertEquals("package io.vlingo.schemata.event;\n" +
+            "\n" +
             "import io.vlingo.lattice.model.DomainEvent;\n" +
             "\n" +
             "public final class SchemaDefined extends DomainEvent {\n" +
-            "  public final SchemaDefined() {\n" +
+            "  public SchemaDefined() {\n" +
             "  }\n" +
-            "}\n"));
+            "}\n",
+            response.entity.content());
   }
 
   @Before