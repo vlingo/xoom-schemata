5a21c94e8862f2b9a25050ec4fa0d0a5ea87a844
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-20 23:17:42 +0100

    Introduce schemata server configuration properties

--- src/main/java/io/vlingo/schemata/Bootstrap.java
+++ src/main/java/io/vlingo/schemata/Bootstrap.java
@@ -7,10 +7,6 @@
 
 package io.vlingo.schemata;
 
-import static io.vlingo.schemata.Schemata.StageName;
-
-import java.time.LocalDateTime;
-
 import io.vlingo.actors.Stage;
 import io.vlingo.actors.World;
 import io.vlingo.common.identity.IdentityGeneratorType;
@@ -22,112 +18,129 @@ import io.vlingo.lattice.grid.GridAddressFactory;
 import io.vlingo.lattice.model.object.ObjectTypeRegistry;
 import io.vlingo.schemata.infra.persistence.SchemataObjectStore;
 import io.vlingo.schemata.query.Queries;
-import io.vlingo.schemata.resource.CodeResource;
-import io.vlingo.schemata.resource.ContextResource;
-import io.vlingo.schemata.resource.OrganizationResource;
-import io.vlingo.schemata.resource.SchemaResource;
-import io.vlingo.schemata.resource.SchemaVersionResource;
-import io.vlingo.schemata.resource.UiResource;
-import io.vlingo.schemata.resource.UnitResource;
+import io.vlingo.schemata.resource.*;
 import io.vlingo.symbio.BaseEntry.TextEntry;
 import io.vlingo.symbio.State.TextState;
 import io.vlingo.symbio.store.object.ObjectStore;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+
+import static io.vlingo.schemata.Schemata.StageName;
+
 public class Bootstrap {
-  public static final int SCHEMATA_PORT = 9019;
 
-  private static Bootstrap instance;
-  private final int port;
-  private final Server server;
-  private final World world;
+    private static Bootstrap instance;
+    private final int port;
+    private final Server server;
+    private final World world;
 
-  public Bootstrap(final String runtimeType) throws Exception {
-    world = World.startWithDefaults("vlingo-schemata");
-    world.stageNamed(StageName, Grid.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
+    public Bootstrap(final String runtimeType) throws Exception {
+        SchemataConfig config = SchemataConfig.forRuntime(runtimeType);
 
-    final NoopDispatcher<TextEntry, TextState> dispatcher = new NoopDispatcher<>();
+        world = World.startWithDefaults("vlingo-schemata");
+        world.stageNamed(StageName, Grid.class, new GridAddressFactory(IdentityGeneratorType.RANDOM));
 
-    final SchemataObjectStore schemataObjectStore = SchemataObjectStore.instance(runtimeType);
-    final ObjectStore objectStore = schemataObjectStore.objectStoreFor(world, dispatcher, schemataObjectStore.persistentMappers());
-    final ObjectTypeRegistry registry = new ObjectTypeRegistry(world);
-    schemataObjectStore.register(registry, objectStore);
+        final NoopDispatcher<TextEntry, TextState> dispatcher = new NoopDispatcher<>();
 
-    final Stage stage = world.stageNamed(StageName);
+        final SchemataObjectStore schemataObjectStore = SchemataObjectStore.instance(config);
+        final ObjectStore objectStore = schemataObjectStore.objectStoreFor(world, dispatcher, schemataObjectStore.persistentMappers());
+        final ObjectTypeRegistry registry = new ObjectTypeRegistry(world);
+        schemataObjectStore.register(registry, objectStore);
 
-    Queries.startAll(stage, objectStore);
+        final Stage stage = world.stageNamed(StageName);
 
-    final OrganizationResource organizationResource = new OrganizationResource(world);
-    final UnitResource unitResource = new UnitResource(world);
-    final ContextResource contextResource = new ContextResource(world);
-    final SchemaResource schemaResource = new SchemaResource(world);
-    final SchemaVersionResource schemaVersionResource = new SchemaVersionResource(world);
-    final CodeResource codeResource = new CodeResource(world);
-    final UiResource uiResource = new UiResource(world);
+        Queries.startAll(stage, objectStore);
 
-    Resources allResources = Resources.are(
-            organizationResource.routes(),
-            unitResource.routes(),
-            contextResource.routes(),
-            schemaResource.routes(),
-            schemaVersionResource.routes(),
-            codeResource.routes(),
-            uiResource.routes()
-    );
+        final OrganizationResource organizationResource = new OrganizationResource(world);
+        final UnitResource unitResource = new UnitResource(world);
+        final ContextResource contextResource = new ContextResource(world);
+        final SchemaResource schemaResource = new SchemaResource(world);
+        final SchemaVersionResource schemaVersionResource = new SchemaVersionResource(world);
+        final CodeResource codeResource = new CodeResource(world);
+        final UiResource uiResource = new UiResource(world);
 
-    port = isDevRuntime(runtimeType) ? SCHEMATA_PORT + LocalDateTime.now().getSecond() + 1 : SCHEMATA_PORT;
+        Resources allResources = Resources.are(
+                organizationResource.routes(),
+                unitResource.routes(),
+                contextResource.routes(),
+                schemaResource.routes(),
+                schemaVersionResource.routes(),
+                codeResource.routes(),
+                uiResource.routes()
+        );
 
-    server = Server.startWith(world.stage(),
-      allResources,
-      port,
-      Configuration.Sizing.define()
-          .withDispatcherPoolSize(2)
-          .withMaxBufferPoolSize(100)
-          .withMaxMessageSize(4096),
-      Configuration.Timing.define());
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      if (instance != null) {
+        port = config.randomPort ? nextFreePort(9019, 9100) : config.serverPort;
+
+        server = Server.startWith(world.stage(),
+                allResources,
+                port,
+                Configuration.Sizing.define()
+                        .withDispatcherPoolSize(2)
+                        .withMaxBufferPoolSize(100)
+                        .withMaxMessageSize(4096),
+                Configuration.Timing.define());
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (instance != null) {
+                instance.server.stop();
+
+                System.out.println("\n");
+                System.out.println("=========================");
+                System.out.println("Stopping vlingo-schemata.");
+                System.out.println("=========================");
+            }
+        }));
+    }
+
+    static Bootstrap instance(final String runtimeType) throws Exception {
+        if (instance == null) {
+            instance = new Bootstrap(runtimeType);
+        }
+        return instance;
+    }
+
+    void stopServer() throws Exception {
+        if (instance == null) {
+            throw new IllegalStateException("Schemata server not running");
+        }
         instance.server.stop();
-
-        System.out.println("\n");
-        System.out.println("=========================");
-        System.out.println("Stopping vlingo-schemata.");
-        System.out.println("=========================");
-      }
-    }));
-  }
-
-  static Bootstrap instance(final String runtimeType) throws Exception {
-    if (instance == null) {
-      instance = new Bootstrap(runtimeType);
     }
-    return instance;
-  }
 
-  void stopServer() throws Exception {
-    if (instance == null) {
-      throw new IllegalStateException("Schemata server not running");
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("=========================");
+        System.out.println("service: vlingo-schemata.");
+        System.out.println("=========================");
+        Bootstrap.instance(args[0]);
     }
-    instance.server.stop();
-  }
 
+    public int __internal__only_test_port() {
+        return port;
+    }
 
+    public World __internal__only_test_world() {
+        return world;
+    }
 
-  public static void main(final String[] args) throws Exception {
-    System.out.println("=========================");
-    System.out.println("service: vlingo-schemata.");
-    System.out.println("=========================");
-    Bootstrap.instance(args[0]);
-  }
+    private int nextFreePort(int from, int to) throws IOException {
+        int port = from;
+        while (port < to) {
+            if (isPortFree(port)) {
+                return port;
+            } else {
+                port++;
+            }
+        }
+        throw new IOException("No open port in range " + from + " to " + to);
+    }
 
-  public int __internal__only_test_port() {
-    return port;
-  }
-
-  public World __internal__only_test_world() {
-    return world;
-  }
-
-  private boolean isDevRuntime(final String runtimeType) {
-    return "dev".equals(runtimeType);
-  }
+    private boolean isPortFree(int port) {
+        try {
+            new ServerSocket(port).close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 }
