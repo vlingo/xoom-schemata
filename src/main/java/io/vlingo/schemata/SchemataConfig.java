dc78a0e50f8a9ba7735ac656e60550a4a2f86cfc
VaughnVernon <vaughn@forcomprehension.com>
2020-01-07 16:06:40 -0700

    Copyright.

--- src/main/java/io/vlingo/schemata/SchemataConfig.java
+++ src/main/java/io/vlingo/schemata/SchemataConfig.java
@@ -1,10 +1,16 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.schemata;
 
-import io.vlingo.common.config.EnvVarProperties;
-
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+
+import io.vlingo.common.config.EnvVarProperties;
 
 public class SchemataConfig {
     public final boolean randomPort;
