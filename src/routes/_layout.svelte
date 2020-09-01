4fe73d60e395f1b57097a79e10c37c6d439319ae
Florian Schönherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-09-01 22:00:27 +0200

    Use sveltestrap Navbar

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,5 @@
 <script>
 	import Nav from '../components/Nav.svelte';
-
-	export let segment;
 </script>
 
 <style>
@@ -15,7 +13,7 @@
 	}
 </style>
 
-<Nav {segment}/>
+<Nav/>
 
 <main>
 	<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,25 +4,7 @@
 	import Row from 'sveltestrap/src/Row.svelte';
 	import { Card, CardBody, Form, FormGroup, FormText, Input, Label, CustomInput } from 'sveltestrap/src';
 	
-	import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
-  } from 'sveltestrap/src';
-
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
+	
 
   let selectText = "";
   let selectInvalid;
@@ -57,29 +39,7 @@
 </svelte:head>
 
 
-<Navbar color="light" light expand="md">
-	<NavbarBrand href="/">sveltestrap</NavbarBrand>
-	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-	  <Nav vertical class="ml-auto" navbar>
-		<NavItem>
-		  <NavLink href="#components/">Components</NavLink>
-		</NavItem>
-		<NavItem>
-		  <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
-		</NavItem>
-		<UncontrolledDropdown nav inNavbar>
-		  <DropdownToggle nav caret>Options</DropdownToggle>
-		  <DropdownMenu right>
-			<DropdownItem>Option 1</DropdownItem>
-			<DropdownItem>Option 2</DropdownItem>
-			<DropdownItem divider />
-			<DropdownItem>Reset</DropdownItem>
-		  </DropdownMenu>
-		</UncontrolledDropdown>
-	  </Nav>
-	</Collapse>
-  </Navbar>
+
 
 
 
