96696d84df78699359a089ac5492cef8e59065bc
Florian Schönherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-09-18 19:35:53 +0200

    Fix initial data GET, add schema version GET

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -11,20 +11,27 @@
 		let contexts = [];
 
 		let schemas = [];
+
+		let schemaVersions = [];
 		
 		for(const org of orgs) {
 			const orgUnits = await SchemataRepository.getUnits(org.organizationId);
 			if(orgUnits) {
-				console.log({orgUnits});
 				units.push(...orgUnits);
-				for(const unit of units) {
+				for(const unit of units.filter(u => u.organizationId == org.organizationId)) {
 					const unitContexts = await SchemataRepository.getContexts(org.organizationId, unit.unitId);
 					if(unitContexts) {
 						contexts.push(...unitContexts);
-						for(const context of contexts) {
+						for(const context of contexts.filter(c => c.unitId == unit.unitId)) {
 							const contextSchemas = await SchemataRepository.getSchemata(org.organizationId, unit.unitId, context.contextId);
 							if(contextSchemas) {
 								schemas.push(...contextSchemas);
+								for(const schema of schemas.filter(s => s.contextId == context.contextId)) {
+									const schemaSchemaVersions = await SchemataRepository.getVersions(org.organizationId, unit.unitId, context.contextId, schema.schemaId);
+									if(schemaSchemaVersions) {
+										schemaVersions.push(...schemaSchemaVersions);
+									}
+								}
 							}
 						}
 					}
@@ -32,46 +39,58 @@
 			}
 		}
 
-		return { orgs, units, contexts, schemas };
+		return { orgs, units, contexts, schemas, schemaVersions };
 		}
 	}
 </script>
 
 <script>
 	import Nav from '../components/Nav.svelte';
-	import { contextsStore, contextStore, firstPage, organizationsStore, organizationStore, schemasStore, schemaStore, unitsStore, unitStore } from '../stores';
+	import { contextsStore, contextStore, firstPage, organizationsStore, organizationStore, schemasStore, schemaStore, schemaVersionsStore, schemaVersionStore, unitsStore, unitStore } from '../stores';
 
 	export let orgs;
 	export let units;
 	export let contexts;
 	export let schemas;
+	export let schemaVersions;
 	
 	if($firstPage) {
 		console.log({$firstPage}, "BEFORE");
-		console.log(orgs, units, contexts, schemas);
-
+		console.log(orgs, units, contexts, schemas, schemaVersions);
+		console.log($contextsStore, $contextStore, $organizationsStore, $organizationStore, $schemasStore, $schemaStore, $schemaVersionsStore, $schemaVersionStore, $unitsStore, $unitStore );
 		// maybe need to check if empty
+		// array reset is maybe not needed
 		// array[0] will be undefined if none exist
 		if(orgs) {
+			$organizationsStore = [];
 			$organizationsStore.push(...orgs);
 			$organizationStore = orgs[0];
 		}
 		if(units) {
+			$unitsStore = [];
 			$unitsStore.push(...units);
 			$unitStore = units[0];
 		}
 		if(contexts) {
+			$contextsStore = [];
 			$contextsStore.push(...contexts);
 			$contextStore = contexts[0];
 		}
 		if(schemas) {
+			$schemasStore = [];
 			$schemasStore.push(...schemas);
 			$schemaStore = schemas[0];
+		}
+		if(schemaVersions) {
+			$schemaVersionsStore = [];
+			$schemaVersionsStore.push(...schemaVersions);
+			$schemaVersionStore = schemaVersions[0];
 		}
 
 		$firstPage = false;
 
 		console.log({$firstPage}, "AFTER");
+		console.log($contextsStore, $contextStore, $organizationsStore, $organizationStore, $schemasStore, $schemaStore, $schemaVersionsStore, $schemaVersionStore, $unitsStore, $unitStore );
 	}
 
 </script>
