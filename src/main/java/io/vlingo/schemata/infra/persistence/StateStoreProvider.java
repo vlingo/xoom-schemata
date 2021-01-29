be0e750062e44be38408f09456c9ad32592b4d95
VaughnVernon <vaughn@kalele.io>
2021-01-29 00:10:17 -0700

    Use InitializationPrimer to unhack StateStore startup.

--- src/main/java/io/vlingo/schemata/infra/persistence/StateStoreProvider.java
+++ src/main/java/io/vlingo/schemata/infra/persistence/StateStoreProvider.java
@@ -9,6 +9,7 @@ package io.vlingo.schemata.infra.persistence;
 
 import java.util.Arrays;
 
+import io.vlingo.actors.ActorInstantiator;
 import io.vlingo.actors.World;
 import io.vlingo.lattice.model.stateful.StatefulTypeRegistry;
 import io.vlingo.lattice.model.stateful.StatefulTypeRegistry.Info;
@@ -31,20 +32,19 @@ import io.vlingo.symbio.store.DataFormat;
 import io.vlingo.symbio.store.common.jdbc.Configuration;
 import io.vlingo.symbio.store.common.jdbc.postgres.PostgresConfigurationProvider;
 import io.vlingo.symbio.store.state.StateStore;
+import io.vlingo.symbio.store.state.StateStore.InitializationPrimer;
 import io.vlingo.symbio.store.state.inmemory.InMemoryStateStoreActor;
 import io.vlingo.symbio.store.state.jdbc.JDBCEntriesInstantWriter;
-import io.vlingo.symbio.store.state.jdbc.JDBCEntriesWriter;
 import io.vlingo.symbio.store.state.jdbc.JDBCStateStoreActor;
+import io.vlingo.symbio.store.state.jdbc.JDBCStateStoreActor.JDBCStateStoreInstantiator;
 import io.vlingo.symbio.store.state.jdbc.JDBCStorageDelegate;
 import io.vlingo.symbio.store.state.jdbc.postgres.PostgresStorageDelegate;
 
 public class StateStoreProvider {
     public final StateStore stateStore;
 
-    @SuppressWarnings({ "rawtypes" })
-    public static StateStoreProvider using(World world, final SchemataConfig config) throws Exception {
-        final StatefulTypeRegistry registry = new StatefulTypeRegistry(world);
-
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static StateStoreProvider using(final World world, final SchemataConfig config) throws Exception {
         final StateStore stateStore;
 
         if (config.isProductionRuntimeType()) {
@@ -58,70 +58,59 @@ public class StateStoreProvider {
                           config.databaseOriginator,
                           true);
 
-//          final JDBCDispatcherControlDelegate dispatcherControlDelegate =
-//                  new JDBCDispatcherControlDelegate(Configuration.cloneOf(postgresConfiguration), world.defaultLogger());
-
-//          final List<Dispatcher<Dispatchable<Entry<String>, TextState>>> dispatchers =
-//                  Collections.singletonList(typed(new NoopDispatcher()));
-
-//          final DispatcherControl dispatcherControl = world.stage().actorFor(DispatcherControl.class,
-//                  Definition.has(DispatcherControlActor.class,
-//                          new DispatcherControl.DispatcherControlInstantiator(
-//                                  dispatchers,
-//                                  dispatcherControlDelegate,
-//                                  config.confirmationExpirationInterval,
-//                                  config.confirmationExpiration)));
-
           final JDBCStorageDelegate<?> delegate = new PostgresStorageDelegate(postgresConfiguration, world.defaultLogger());
 
-          final JDBCEntriesWriter entriesWriter = new JDBCEntriesInstantWriter(typed(delegate), null, null);
+          final JDBCEntriesInstantWriter entriesWriter = new JDBCEntriesInstantWriter(typed(delegate), null, null);
 
-          // TODO: fix this hack. makes store names register before JDBCStateStoreActor
-          // is created so that tables can be created by store names. This will be
-          // overwritten below when it is run again with the actual store.
-          registerStatefulTypes(null, registry);
+          final ActorInstantiator instantiator = new JDBCStateStoreInstantiator(typed(delegate), entriesWriter, new StateStoreInitializationPrimer(world));
 
-          stateStore = world.actorFor(StateStore.class, JDBCStateStoreActor.class, typed(delegate), entriesWriter);
+          stateStore = world.actorFor(StateStore.class, JDBCStateStoreActor.class, instantiator);
 
         } else {
-          stateStore = world.stage().actorFor(StateStore.class,
-                  InMemoryStateStoreActor.class,
-                  Arrays.asList(new NoopDispatcher()));
+          stateStore = world.stage().actorFor(StateStore.class, InMemoryStateStoreActor.class, Arrays.asList(new NoopDispatcher()));
         }
-
-        registerStatefulTypes(stateStore, registry);
 
         return new StateStoreProvider(stateStore);
     }
 
-    private static void registerStatefulTypes(StateStore stateStore, StatefulTypeRegistry registry) {
-        registry
-                .register(new Info<>(stateStore, OrganizationView.class, OrganizationView.class.getSimpleName()))
-                .register(new Info<>(stateStore, OrganizationsView.class, OrganizationsView.class.getSimpleName()))
-                .register(new Info<>(stateStore, UnitView.class, UnitView.class.getSimpleName()))
-                .register(new Info<>(stateStore, UnitsView.class, UnitsView.class.getSimpleName()))
-                .register(new Info<>(stateStore, ContextView.class, ContextView.class.getSimpleName()))
-                .register(new Info<>(stateStore, ContextsView.class, ContextsView.class.getSimpleName()))
-                .register(new Info<>(stateStore, SchemaView.class, SchemaView.class.getSimpleName()))
-                .register(new Info<>(stateStore, SchemasView.class, SchemasView.class.getSimpleName()))
-                .register(new Info<>(stateStore, SchemaVersionView.class, SchemaVersionView.class.getSimpleName()))
-                .register(new Info<>(stateStore, SchemaVersionsView.class, SchemaVersionsView.class.getSimpleName()))
-                .register(new Info<>(stateStore, NamedSchemaView.class, NamedSchemaView.class.getSimpleName()))
-                .register(new Info<>(stateStore, CodeView.class, CodeView.class.getSimpleName()));
-    }
-
-//    private static void registerStateAdapters(Stage stage) {
-//        final StateAdapterProvider stateAdapterProvider = new StateAdapterProvider(stage.world());
-//        stateAdapterProvider.registerAdapter(OrganizationView.class, new OrganizationStateAdapter());
-//        new EntryAdapterProvider(stage.world()); // future?
-//    }
-
-    private StateStoreProvider(StateStore stateStore) {
+    private StateStoreProvider(final StateStore stateStore) {
         this.stateStore = stateStore;
     }
 
     @SuppressWarnings({"unchecked"})
     private static JDBCStorageDelegate<State.TextState> typed(final JDBCStorageDelegate<?> delegate) {
         return (JDBCStorageDelegate<State.TextState>) delegate;
+    }
+
+    public static class StateStoreInitializationPrimer implements InitializationPrimer {
+      private final World world;
+
+      StateStoreInitializationPrimer(final World world) {
+        this.world = world;
+      }
+
+      @Override
+      public void prime(final StateStore stateStore) {
+        registerStatefulTypes(stateStore);
+      }
+
+      private void registerStatefulTypes(final StateStore stateStore) {
+        world.defaultLogger().info("=============== PRE-INITIALIZATION ===============");
+        final StatefulTypeRegistry registry = new StatefulTypeRegistry(world);
+
+        registry
+                  .register(new Info<>(stateStore, OrganizationView.class, OrganizationView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, OrganizationsView.class, OrganizationsView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, UnitView.class, UnitView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, UnitsView.class, UnitsView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, ContextView.class, ContextView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, ContextsView.class, ContextsView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, SchemaView.class, SchemaView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, SchemasView.class, SchemasView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, SchemaVersionView.class, SchemaVersionView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, SchemaVersionsView.class, SchemaVersionsView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, NamedSchemaView.class, NamedSchemaView.class.getSimpleName()))
+                  .register(new Info<>(stateStore, CodeView.class, CodeView.class.getSimpleName()));
+      }
     }
 }
