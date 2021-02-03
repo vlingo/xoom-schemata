bd5d07081b20b51aa1b8ce619980dfa2f84dfc65
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-03 23:20:06 +0300

    unit alert is updated
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/alerts/UnitAlert.svelte
+++ src/main/frontend/src/components/alerts/UnitAlert.svelte
@@ -3,15 +3,4 @@
 	export let notChosenAlert = false;
 </script>
 
-{#if notChosenAlert}
-	<Alert>
-		<div slot="heading">Choose a Unit</div>
-		Choose a Unit in the tree.
-	</Alert>
-{:else}
-	<Alert link={"unit"}>
-		<div slot="heading">Define a Unit</div>
-		When you have defined a unit, it will be shown inside the tree.
-		<div slot="link-text">Click here to define a Unit.</div>
-	</Alert>
-{/if}
+<Alert name="Unit" link={!notChosenAlert ? "unit" : ""} />
