b82f5572bfe72ede31ade05e2cdb1e9f3632ab84
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-02-03 23:18:50 +0300

    alert component is updated
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/alerts/Alert.svelte
+++ src/main/frontend/src/components/alerts/Alert.svelte
@@ -1,13 +1,31 @@
 <script>
 	import { Alert } from 'svelte-materialify/src';
-	export let link = "";
+	export let name;
+	export let link;
+	$: headingText = link ? `Define an ${name}` : `Choose an ${name}`;
+	$: paragraphText = link ? `When you have defined an ${name.toLowerCase()}, it will be shown inside a tree right here.` : `Choose an ${name} in the tree.`
+	$: linkText = link ? `Click here to start by defining an ${name}.` : ``
 </script>
 
 
 <Alert transitionOpts={{ duration: 100 }} class="info-color" outlined>
-	<h4 class="alert-heading"><slot name="heading"></slot></h4>
-	<p><slot/></p>
+	<h4 class="alert-heading">
+		<slot name="heading">
+			{headingText}
+		</slot>
+	</h4>
+	<p>
+		<slot>
+			{paragraphText}
+		</slot>
+	</p>
 	{#if link}
-		<p><a href={link} class="alert-link"><slot name="link-text"></slot></a></p>
+		<p>
+			<a href={link} class="alert-link">
+				<slot name="link-text">
+					{linkText}
+				</slot>
+			</a>
+		</p>
 	{/if}
 </Alert>
