2b9df7b4335b7e75ec6b59a9dea22014f1c094e2
Jakub Zalas <jakub@zalas.pl>
2021-04-16 17:06:18 +0100

    Improve tests to verify behaviour and show intent
    Tests now verify if any outcome was actually returned. Previously no data was actually put into the state store.
    
    Tests now docuemnt the intent better. Instead of:
    
    ```
    queries.schemaByNames("OO", "UU", "CC", "SS")
    ```
    
    we now have:
    
    ```
    queries.schemaByNames("vlingo", "schemata-test", "io.vlingo.xoom.schemata", "SchemaDefined")
    ```
    
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- src/test/java/io/vlingo/xoom/schemata/queries/SchemaQueriesTest.java
+++ src/test/java/io/vlingo/xoom/schemata/queries/SchemaQueriesTest.java
@@ -3,16 +3,28 @@ package io.vlingo.xoom.schemata.queries;
 import io.vlingo.xoom.actors.World;
 import io.vlingo.xoom.actors.testkit.TestWorld;
 import io.vlingo.xoom.common.Completes;
+import io.vlingo.xoom.common.Outcome;
 import io.vlingo.xoom.lattice.model.stateful.StatefulTypeRegistry;
+import io.vlingo.xoom.schemata.Schemata;
+import io.vlingo.xoom.schemata.model.Category;
+import io.vlingo.xoom.schemata.model.Scope;
 import io.vlingo.xoom.schemata.query.SchemaQueries;
 import io.vlingo.xoom.schemata.query.SchemaQueriesActor;
 import io.vlingo.xoom.schemata.query.view.NamedSchemaView;
+import io.vlingo.xoom.schemata.query.view.SchemaView;
+import io.vlingo.xoom.symbio.Source;
+import io.vlingo.xoom.symbio.store.Result;
+import io.vlingo.xoom.symbio.store.StorageException;
 import io.vlingo.xoom.symbio.store.dispatch.NoOpDispatcher;
+import io.vlingo.xoom.symbio.store.state.StateStore;
 import io.vlingo.xoom.symbio.store.state.inmemory.InMemoryStateStoreActor;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,6 +33,36 @@ public class SchemaQueriesTest {
   private World world;
   private SchemaQueries queries;
   private CountingStateStore stateStore;
+
+  @Test
+  public void schemaByNamesReturnsTheSchemaIfFound() {
+    givenNamedSchemaView("vlingo", "schemata-test", "io.vlingo.xoom.schemata", "SchemaDefined", "First schema", "My first schema");
+
+    Completes<NamedSchemaView> query = queries.schemaByNames("vlingo", "schemata-test", "io.vlingo.xoom.schemata", "SchemaDefined");
+    NamedSchemaView view = query.await();
+
+    assertEquals("First schema", view.name());
+  }
+
+  @Test
+  public void schemaByNamesReturnsNullIfNotFound() {
+    Completes<NamedSchemaView> query = queries.schemaByNames("vlingo", "schemata-test", "io.vlingo.xoom.schemata", "SchemaDefined");
+    NamedSchemaView view = query.await();
+
+    assertEquals(null, view);
+  }
+
+  @Test
+  public void schemaByNamesWithRetriesReturnsTheSchemaDespiteInitialFailures() {
+    givenNamedSchemaView("vlingo", "schemata-test", "io.vlingo.xoom.schemata", "SchemaDefined", "First schema", "My first schema");
+
+    Completes<NamedSchemaView> query = queries.schemaByNamesWithRetries("OO", "UU", "CC", "SS", 100, 3);
+    NamedSchemaView view = query.await();
+    int readCount = stateStore.getReadCount();
+
+    assertEquals(3, readCount);
+    assertEquals("First schema", view.name());
+  }
 
   @Before
   public void init() {
@@ -31,19 +73,29 @@ public class SchemaQueriesTest {
     queries = world.actorFor(SchemaQueries.class, SchemaQueriesActor.class, stateStore);
   }
 
-  @Test
-  public void schemaByNames() {
-    Completes<NamedSchemaView> res = queries.schemaByNames("OO", "UU", "CC", "SS");
-    res.await();
-    int readCount = stateStore.getReadCount();
-    assertEquals(1, readCount);
+  @After
+  public void shutdown() {
+    if (world != null) {
+      world.terminate();
+    }
   }
 
-  @Test
-  public void schemaByNamesWithRetries() {
-    Completes<NamedSchemaView> res = queries.schemaByNamesWithRetries("OO", "UU", "CC", "SS", 100, 3);
-    res.await();
-    int readCount = stateStore.getReadCount();
-    assertEquals(3, readCount);
+  private void givenNamedSchemaView(String organizationId, String unitId, String contextId, String schemaId, String name, String description) {
+    String reference = Arrays.asList(organizationId, unitId, contextId, schemaId).stream().collect(Collectors.joining(Schemata.ReferenceSeparator));
+    stateStore.write(
+            reference,
+            NamedSchemaView.with(
+                    reference,
+                    SchemaView.with(organizationId, unitId, contextId, schemaId, Category.Event, Scope.Public, name, description)
+            ),
+            1,
+            new NoOpWriteResultInterest()
+    );
+  }
+
+  private class NoOpWriteResultInterest implements StateStore.WriteResultInterest {
+    @Override
+    public <S, C> void writeResultedIn(Outcome<StorageException, Result> outcome, String s, S s1, int i, List<Source<C>> list, Object o) {
+    }
   }
 }
