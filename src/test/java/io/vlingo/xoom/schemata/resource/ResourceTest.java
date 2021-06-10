5261ee99a86fa44ad97ee4f48e0b80584d4ca273
Jakub Zalas <jakub@zalas.pl>
2021-06-10 14:56:38 +0200

    Start the grid in resource tests

--- src/test/java/io/vlingo/xoom/schemata/resource/ResourceTest.java
+++ src/test/java/io/vlingo/xoom/schemata/resource/ResourceTest.java
@@ -7,10 +7,9 @@
 
 package io.vlingo.xoom.schemata.resource;
 
-import org.junit.After;
-import org.junit.Before;
-
+import io.vlingo.xoom.actors.Configuration;
 import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.cluster.ClusterProperties;
 import io.vlingo.xoom.http.Response;
 import io.vlingo.xoom.http.ResponseHeader;
 import io.vlingo.xoom.lattice.grid.Grid;
@@ -20,14 +19,10 @@ import io.vlingo.xoom.schemata.SchemataConfig;
 import io.vlingo.xoom.schemata.infra.persistence.ProjectionDispatcherProvider;
 import io.vlingo.xoom.schemata.infra.persistence.StateStoreProvider;
 import io.vlingo.xoom.schemata.infra.persistence.StorageProvider;
-import io.vlingo.xoom.schemata.query.CodeQueries;
-import io.vlingo.xoom.schemata.query.ContextQueries;
-import io.vlingo.xoom.schemata.query.OrganizationQueries;
-import io.vlingo.xoom.schemata.query.SchemaQueries;
-import io.vlingo.xoom.schemata.query.SchemaVersionQueries;
-import io.vlingo.xoom.schemata.query.UnitQueries;
+import io.vlingo.xoom.schemata.query.*;
 import io.vlingo.xoom.symbio.store.journal.Journal;
-import io.vlingo.xoom.turbo.Boot;
+import org.junit.After;
+import org.junit.Before;
 
 public abstract class ResourceTest {
   protected Journal<String> journal;
@@ -44,8 +39,7 @@ public abstract class ResourceTest {
 
   @Before
   public void setUp() throws Exception {
-    // TODO: Start an actual Grid here using Grid.start(...). Needs a test grid configuration first
-    stage = Boot.start("test-command-router", Schemata.NodeName);
+    stage = Grid.start("test-command-router", Configuration.define(), ClusterProperties.oneNode(), Schemata.NodeName);
     world = stage.world();
 
     final SchemataConfig config = SchemataConfig.forRuntime(SchemataConfig.RUNTIME_TYPE_DEV);
